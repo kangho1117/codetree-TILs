b449c220e7c32d1dd0470f0a744c43b6a3a498a9
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-07-19 15:31:34 +0900

    Title: 수열에서의 증가와 감소, Time: 35ms, Memory: 0MB, Status: Passed - CodeTree

--- 240719/수열에서의 증가와 감소/increase-and-decrease-in-sequence.cpp
+++ 240719/수열에서의 증가와 감소/increase-and-decrease-in-sequence.cpp
@@ -3,17 +3,32 @@ using namespace std;
 
 int main() {
     int n,input, max=0, cnt=1;
+    bool up = true;
     cin>>n;
     int arr[n];
     for(int i=0;i<n;i++)
         cin>>arr[i];
     for(int i=0;i<n-1;i++)
     {
-        if(arr[i] <= arr[i+1]) cnt++;
+        if(arr[i] <= arr[i+1]) 
+        {
+            if(up == false) 
+            {
+                cnt = 1;
+                up = true;
+            }
+            cnt++;
+            if(max < cnt) max = cnt;
+        }
         else 
         {
+            if(up == true)
+            {
+                cnt = 1;
+                up = false;
+            }
+            cnt++;
             if(max < cnt) max = cnt;
-            cnt = 1;
         }
     }
     cout<<max;
