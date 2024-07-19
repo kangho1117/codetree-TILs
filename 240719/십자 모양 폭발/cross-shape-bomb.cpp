859ecd6123e010fdd3562faa5d7d96f7e2ad9b36
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-07-19 10:07:08 +0900

    Title: 십자 모양 폭발, Time: 13ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240719/십자 모양 폭발/cross-shape-bomb.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+vector<vector<int>> gravity(vector<vector<int>> &vec, int n)
+{
+    vector<vector<int>> temp_2d(n+1, vector<int>(n+1,0));
+    for(int j=1;j<=n;j++)
+    {
+        vector<int> temp(1,0);
+        for(int i = 1; i <= n; i++)
+        {
+            if(vec[i][j] != 0)
+            {
+                temp.push_back(vec[i][j]);
+            }
+        }
+        while(temp.size() < n+1)
+            temp.insert(temp.begin(),0);
+        for(int i = 1; i <= n; i++)
+        {
+            temp_2d[i][j] = temp[i];
+        }
+    }
+    return temp_2d;
+}
+int main() {
+    int n, r, c;
+    cin>>n;
+    vector<vector<int>> vec(n+2,vector<int>(n+2,-1));
+    for(int i=1;i<=n;i++)
+        for(int j=1;j<=n;j++)
+            cin>>vec[i][j];
+    cin>>r>>c;
+    int boom = vec[r][c];
+    for(int i=0;i<boom;i++)
+    {
+        if(vec[r+i][c] != -1) vec[r+i][c] = 0;
+        if(vec[r-i][c] != -1) vec[r-i][c] = 0;
+        if(vec[r][c+i] != -1) vec[r][c+i] = 0;
+        if(vec[r][c-i] != -1) vec[r][c-i] = 0;
+    }
+
+    vec = gravity(vec, n);
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=n;j++)
+        {
+            cout<<vec[i][j]<<" ";
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
