f0d129f58fde301c374c87010c0709c61d6acb20
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-07-21 22:16:39 +0900

    Title: 1차원 폭발 게임, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240721/1차원 폭발 게임/The-1D-bomb-game.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int n,m;
+vector<int> zero(vector<int> &vec)
+{
+    vector<int> nvec;
+    for(int i=0;i<vec.size();i++)
+    {
+        if(vec[i] != 0) nvec.push_back(vec[i]);
+    }
+    nvec.push_back(0);
+    return nvec;
+}
+vector<int> remove(vector<int> &vec)
+{
+    vector<int> mvec;
+    int mval=vec[0], cnt=1, idx=0;
+    for(int i=1;i<vec.size();i++)
+    {
+        //미달성 초기화
+        if(cnt < m && vec[i] != mval)
+        {
+            //cout<<"one";
+            cnt = 1;
+            mval = vec[i];
+            idx = i;
+        }
+        //달성 초기화 및 기록
+        else if(cnt >= m && vec[i] != mval)
+        {
+            //cout<<"two";
+            for(int j = idx;j<i;j++)
+            {
+                vec[j] = 0;
+            }
+            idx = i;
+            cnt = 1;
+            mval = vec[i];
+        }
+        //달성 및 연장
+        else if(cnt >= m && vec[i] == mval)
+        {
+            //cout<<"three";
+            cnt++;
+        }
+        //미달성 및 연장
+        else if(cnt < m && vec[i] == mval)
+        {
+            //cout<<"four";
+            cnt++;
+        }
+    }
+    return zero(vec);
+}
+
+
+int main() {
+
+    cin>>n>>m;
+    vector<int> vec(n);
+    for(int i=0;i<n;i++)
+        cin>>vec[i];
+    vec.push_back(0);
+    while(1)
+    {
+        int size = -1;
+        vec = remove(vec);
+        size = vec.size();
+        vec = remove(vec);
+        if(size == vec.size()) break;
+    }
+    vec.pop_back();
+    cout<<vec.size()<<"\n";
+    for(auto a : vec)
+        cout<<a<<"\n";
+    return 0;
+}
