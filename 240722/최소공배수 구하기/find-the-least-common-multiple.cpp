ba0d2bfc455020ddd64a01aa10f37913094a57f9
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-07-22 14:55:51 +0900

    Title: 최소공배수 구하기, Time: 13ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240722/최소공배수 구하기/find-the-least-common-multiple.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n,m,a,b;
+    cin>>n>>m;
+    if(n<m)
+    {
+        a = m;
+        b = n;
+    }
+    int c = a%b;
+    while(c != 0)
+    {
+        a = b;
+        b = c;
+        c = a % b;
+    }
+    cout <<n*m/b;
+    return 0;
+}
