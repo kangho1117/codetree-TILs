caa047cdbcc5bbf993e880e9594837c3e9eef812
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-07-23 10:51:04 +0900

    Title: 함수를 이용한 온전수 판별, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240723/함수를 이용한 온전수 판별/determining-the-whole-number-using-a-function.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int cal(int a, int b)
+{
+    int cnt=0;
+    for(int i=a;i<=b;i++)
+    {
+        if(i%2==0) continue;
+        else if(i%10==5) continue;
+        else if(i%3==0 && i%9 != 0) continue;
+        else cnt++;
+    }
+    return cnt;
+}
+int main() {
+    int a,b;
+    cin>>a>>b;
+    cout<<cal(a,b);
+    return 0;
+}
