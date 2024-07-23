be650154248be4af6ad8192bab5af02b79a2fae5
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-07-23 13:49:07 +0900

    Title: palindrome 여부 판단하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240723/palindrome 여부 판단하기/determine-whether-palindrome-is-present.cpp
+++ 240723/palindrome 여부 판단하기/determine-whether-palindrome-is-present.cpp
@@ -4,9 +4,7 @@ using namespace std;
 bool check(string &str)
 {
     for(int i=0, j=str.size()-1;i<str.size();i++,j--)
-    {
         if(str[i] != str[j]) return 0;
-    }
     return 1;
 }
 int main() {
