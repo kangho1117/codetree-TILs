0d06a222da51a1dde6f314ffcd1d091b6b374e22
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-07-23 13:34:45 +0900

    Title: 그 계절, 그 날, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240723/그 계절, 그 날/that-season-that-day.cpp
+++ 240723/그 계절, 그 날/that-season-that-day.cpp
@@ -2,51 +2,64 @@
 using namespace std;
 bool yoon(int year)
 {
-    if(year%4 != 0) return false;
-    else if(year%4 == 0 && year%100==0)
+    if(year%4 == 0 && year%100==0)
     {
         if(year%400==0) return true;
         else return false;
     }
-    return true;
+    else if(year%4 == 0) return true;
+    return false;
 }
 void cal(int y, int m, int d)
 {
     if(0<m && m < 13)
     {
-        if(m==2)
+        if(0<d && d < 32)
         {
-            if(yoon(y) && 0 < d &&  d < 30)
+            if(m==2)
             {
-                cout<<"Winter";
+                if(yoon(y))
+                {
+                    if(d<30)
+                    {
+                        cout<<"Winter";
+                    }
+                    else cout<<"-1";
+                }
+                else
+                {
+                    if(d<29)
+                    {
+                        cout<<"Winter";
+                    }
+                    else cout<<"-1";
+                }
             }
-            else if(yoon(y) == false && 0 < d &&  d < 29)
+            else if(m==1 || m==3 || m==5 || m==7 || m==8 || m==10 || m ==12)
             {
-                cout<<"Winter";
+                if(d<32)
+                {
+                    if(m==1) cout<<"Winter";
+                    else if(m==3 || m==5) cout<<"Spring";
+                    else if(m==7 || m==8) cout<<"Summer";
+                    else if(m==10) cout<<"Fall";
+                    else cout<<"Winter";
+                }
+                else cout<<"-1";
             }
+            else
+            {
+                if(d<31)
+                {
+                    if(m==4) cout<<"Spring";
+                    else if(m==6 ) cout<<"Summer";
+                    else if(m==9 || m==11) cout<<"Fall";
+                }
+                else cout<<"-1";
+            }
+
         }
-        else if(m==1 || m==3 || m==5 || m==7 || m==8 || m == 10 || m == 12)
-        {
-            if(0<d && d < 32)
-            {
-                if(3<=m && m<=5) cout<<"Spring";
-                else if(6<=m<=8) cout<<"Summer";
-                else if(9<=m<=11) cout<<"Fall";
-                else if(m== 1 || m == 12) cout<<"Winter";
-            }
-            else cout<<"-1";
-        }
-        else
-        {
-            if(0<d && d < 31)
-            {
-                if(3<=m && m<=5) cout<<"Spring";
-                else if(6<=m<=8) cout<<"Summer";
-                else if(9<=m<=11) cout<<"Fall";
-                else if(m== 1 || m == 12) cout<<"Winter";
-            }
-            else cout<<"-1";
-        }
+        else cout<<"-1";
     }
     else
     {
@@ -57,5 +70,7 @@ int main() {
     int y,m,d;
     cin>>y>>m>>d;
     cal(y,m,d);
+
+
     return 0;
 }
