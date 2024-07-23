4ab223398aa2f6adc75d18293bd21192cc06a8bd
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-07-23 13:11:51 +0900

    Title: 2021년 날짜의 유무, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240723/2021년 날짜의 유무/with-or-without-2021.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+void cal(int m, int d)
+{
+    if(0<m&&m<13)
+    {
+        if(m==2 && 0<d && d<29) 
+        {
+            cout<<"Yes"; 
+            return;
+        }
+        else if((m==1 || m==3 || m==5 || m==7 || m==8 || m==10||m==12) && 0<d && d<32)
+         {
+            cout<<"Yes";
+            return;
+            }
+        else if((m==4 || m==6 || m==9 || m==11) && 0<d && d<31) 
+        {
+            cout<<"Yes";
+            return;
+        }
+            cout<<"No";
+    }
+    cout<<"No";
+}
+int main() {
+    int m,d;
+    cin>>m>>d;
+    cal(m,d);
+    return 0;
+}
