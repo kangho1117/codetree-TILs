514519eb8533ac4ded46a14773c12c7469043540
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-07-24 13:41:26 +0900

    Title: 2개 이상의 알파벳, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240724/2개 이상의 알파벳/more-than-one-alphabet.cpp
+++ 240724/2개 이상의 알파벳/more-than-one-alphabet.cpp
@@ -3,11 +3,9 @@
 using namespace std;
 string search(string str)
 {
-    char a = str[0],b='!';
+    char a = str[0];
     for(int i=1;i<str.size();i++)
-    {
         if(str[i] != a) return "Yes";
-    } 
     return "No";
 }
 int main() {
