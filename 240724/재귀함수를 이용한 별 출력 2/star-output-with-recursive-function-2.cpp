cb33589093f1bcc2bd78f5fe4f8a7faa9e6ef618
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-07-24 17:49:01 +0900

    Title: 재귀함수를 이용한 별 출력 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240724/재귀함수를 이용한 별 출력 2/star-output-with-recursive-function-2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+void star(int n)
+{
+    if(n==0) return;
+    for(int i=0;i<n;i++)
+        cout<<"* ";
+    cout<<"\n";
+    star(n-1);
+    for(int i=0;i<n;i++)
+        cout<<"* ";
+    cout<<"\n";
+}
+int main() {
+    int n;
+    cin>>n;
+    star(n);
+    return 0;
+}
