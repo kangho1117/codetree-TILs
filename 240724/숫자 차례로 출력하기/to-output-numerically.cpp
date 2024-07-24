56fd48cf2facfae5e35f46dc56196d5dde74a0dd
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-07-24 17:38:26 +0900

    Title: 숫자 차례로 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240724/숫자 차례로 출력하기/to-output-numerically.cpp
+++ 240724/숫자 차례로 출력하기/to-output-numerically.cpp
@@ -3,19 +3,20 @@ using namespace std;
 void print1(int n)
 {
     if(n==0) return;
-    cout<<n<<"\n";
+    cout<<n<<" ";
     print1(n-1);
 }
 void print2(int n)
 {
     if(n==0) return;
     print2(n-1);
-    cout<<n<<"\n";
+    cout<<n<<" ";
 }
 int main() {
     int n;
     cin>>n;
-    print1(n);
     print2(n);
+    cout<<"\n";
+    print1(n);
     return 0;
 }
