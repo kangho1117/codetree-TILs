85032f20430efc83503ca2a11418d679bd3f3858
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-07-25 09:10:33 +0900

    Title: Factorial, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240725/Factorial/factorial.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int fac(int n)
+{
+    if(n==0) return 1;
+    return fac(n-1) * n;
+}
+int main() {
+    int n;
+    cin>>n;
+    cout<<fac(n);
+    return 0;
+}
