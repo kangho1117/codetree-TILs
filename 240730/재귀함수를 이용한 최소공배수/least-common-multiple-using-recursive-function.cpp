13d0f5bfb254024e78c5c289ebbc4948d50b3acb
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-07-30 10:22:55 +0900

    Title: 재귀함수를 이용한 최소공배수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240730/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.cpp
+++ 240730/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.cpp
@@ -1,19 +1,18 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-int n;
-vector<int> vec;
-int gcd(int a,int b)
+int gcd(int a, int b)
 {
     if(b==0) return a;
     return gcd(b,a%b);
 }
-int lcm(int a,int b)
+int lcm(int a, int b)
 {
     return (a*b/gcd(a,b));
 }
 int main() {
-    int c,temp;
+    int n,c,temp;
+    vector<int> vec;
     cin>>n;
     for(int i=0;i<n;i++)
     {
