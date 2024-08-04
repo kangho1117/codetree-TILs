bb6c5590ad5d93837d92f1d9231afc015f6c0d8c
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-08-04 15:32:31 +0900

    Title: 비오는 날, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240804/비오는 날/rainy-day.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <tuple>
+#include <vector>
+using namespace std;
+class Day
+{
+public:
+    int year, month, day;
+    string date, weather;
+    Day(int year = 0, int month = 0, int day = 0, string date = "", string weather = "")
+    {
+        this->year = year;
+        this->month = month;
+        this->date = date;
+        this->day = day;
+        this->weather = weather;
+    }
+    void print()
+    {
+        cout << "y : " << year << " m : " << month << " d : " << day << " d : " << date << " w : " << weather << "\n";
+    }
+};
+int main()
+{
+    int n;
+    cin >> n;
+    string a, b, c;
+    vector<Day *> vec;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a >> b >> c;
+        Day *d = new Day(stoi(a.substr(0, 4)), stoi(a.substr(5, 2)), stoi(a.substr(8, 2)), b, c);
+        vec.push_back(d);
+    }
+    int miny = 0, minm = 0, mind = 0;
+    string mindt = "", minw = "";
+    for (int i = 0; i < n; i++)
+    {
+        if (vec[i]->weather == "Rain")
+        {
+            if (miny == 0)
+            {
+                miny = vec[i]->year, minm = vec[i]->month, mind = vec[i]->day,
+                mindt = vec[i]->date, minw = vec[i]->weather;
+            }
+            if (miny > vec[i]->year)
+            {
+                miny = vec[i]->year;
+                minm = vec[i]->month;
+                mind = vec[i]->day;
+                mindt = vec[i]->date;
+                minw = vec[i]->weather;
+            }
+            else if (miny == vec[i]->year && minm > vec[i]->month)
+            {
+                minm = vec[i]->month;
+                mind = vec[i]->day;
+                mindt = vec[i]->date;
+                minw = vec[i]->weather;
+            }
+            else if (miny == vec[i]->year && minm == vec[i]->month && mind > vec[i]->day)
+            {
+                mind = vec[i]->day;
+                mindt = vec[i]->date;
+                minw = vec[i]->weather;
+            }
+        }
+    }
+    cout << miny << "-" << minm << "-" << mind << " " << mindt << " " << minw << "\n";
+    return 0;
+}
