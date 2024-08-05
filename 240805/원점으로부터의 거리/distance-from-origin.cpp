796a356662b50e0daa3cc2fbdf21aec979f51b3e
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-08-05 15:05:03 +0900

    Title: 원점으로부터의 거리, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240805/원점으로부터의 거리/distance-from-origin.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <tuple>
+#include <cmath>
+#include <algorithm>
+using namespace std;
+bool cmp(tuple<int,int,int> &a, tuple<int,int,int> &b)
+{
+    int cal1 = sqrt(pow(get<0>(a),2))+sqrt(pow(get<1>(a),2));
+    int cal2 = sqrt(pow(get<0>(b),2))+sqrt(pow(get<1>(b),2));
+    return cal1 < cal2;
+}
+int main() {
+    int n;
+    cin>>n;
+    tuple<int,int,int> t[n];
+    for(int i=0,a,b;i<n;i++)
+    {
+        cin>>a>>b;
+        t[i] = make_tuple(a,b,i+1);
+    }
+    sort(t,t+n,cmp);
+    for(auto &a : t)
+    cout<<get<2>(a)<<"\n";
+    return 0;
+}
