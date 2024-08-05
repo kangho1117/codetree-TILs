9cf38d64d343f02fcd4bfa90881a31858dc189e4
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-08-05 14:35:09 +0900

    Title: 개인정보, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240805/개인정보/personal-info.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <tuple>
+#include <algorithm>
+using namespace std;
+bool cmp(tuple<string,int,double> &a, tuple<string,int,double> &b)
+{
+    return get<1>(a) > get<1>(b);
+}
+int main() {
+    tuple<string,int,double> t[5];
+    string name;
+    int a;
+    double b;
+    for(int i=0;i<5;i++)
+    {
+        cin>>name>>a>>b;
+        t[i] = make_tuple(name,a,b);
+    }
+    sort(t,t+5);
+    cout<<"name\n";
+    for(auto &a:t)
+    cout<<get<0>(a)<<" "<<get<1>(a)<<" "<<get<2>(a)<<"\n";
+    sort(t,t+5,cmp);
+    cout<<"\nheight\n";
+    for(auto &a:t)
+    cout<<get<0>(a)<<" "<<get<1>(a)<<" "<<get<2>(a)<<"\n";
+    return 0;
+}
