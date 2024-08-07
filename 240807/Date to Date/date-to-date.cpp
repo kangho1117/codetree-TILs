f8ca7dfd092ab96cff98f69ba4d05c3963b09b7c
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-08-07 09:50:17 +0900

    Title: Date to Date, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240807/Date to Date/date-to-date.cpp
+++ 240807/Date to Date/date-to-date.cpp
@@ -5,16 +5,17 @@ int main() {
     int m1,m2,d1,d2,sum_days1=0,sum_days2=0;
     cin>>m1>>d1>>m2>>d2;
     int num_of_days[] = {0,31,28,31,30,31,30,31,31,30,31,30,31};
-    for(int month = 1; month <= m1; month++)
+    for(int month = 1; month < m1; month++)
     {
         sum_days1 += num_of_days[month];
     }
     sum_days1 += d1;
-    for(int month = 1; month <= m2; month++)
+    for(int month = 1; month < m2; month++)
     {
         sum_days2 += num_of_days[month];
     }
-    sum_days2 += d2+1;
-    cout<<sum_days2-sum_days1;
+    sum_days2 += d2;
+    //cout<<sum_days1<<" "<<sum_days2<<"\n";
+    cout<<sum_days2-sum_days1+1;
     return 0;
 }
