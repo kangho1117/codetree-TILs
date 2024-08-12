479940c6ee3869c2fcc879a2005ddec394d62d59
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-08-12 09:42:00 +0900

    Title: 진수 to 진수, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240812/진수 to 진수/transformation-of-number-system.cpp
+++ 240812/진수 to 진수/transformation-of-number-system.cpp
@@ -5,16 +5,16 @@ int main() {
     string n;
     int a,b,sum=0;
     cin>>a>>b>>n;
-    for(int i=n.size()-1;i>=0;i--)
+    for(int i=n.size()-1; i>=0; i--)
         sum = a * sum + n[i]-48;
     n="";
-    while(sum != 1)
+    while(sum != 1 && sum != 0)
     {
         n += to_string(sum%b);
         sum/=b;
     }
-    n+="1";
-    for(int i= n.size()-1;i>=0;i--)
+    if(sum==1) n += to_string(sum);
+    for(int i= n.size()-1; i>=0; i--)
         cout<<n[i];
     return 0;
 }
