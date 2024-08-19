28b1890f8a1e83a9d17b50132a13dd4c5e5f539b
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-08-19 09:13:30 +0900

    Title: 겹치지 않는 사각형의 넓이, Time: 25ms, Memory: 15MB, Status: Runtime Error - CodeTree

--- 240819/겹치지 않는 사각형의 넓이/area-of-non-overlapping-rectangle.cpp
+++ 240819/겹치지 않는 사각형의 넓이/area-of-non-overlapping-rectangle.cpp
@@ -1,24 +1,24 @@
 #include <iostream>
 using namespace std;
 int main() {
-    int arr[2002][2002] = {0,};
+    int arr[2001][2001] = {0,};
     int x1,y1,x2,y2;
     for(int n=0;n<2;n++)
     {
         cin>>x1>>y1>>x2>>y2;
         for(int j=y1+1;j<=y2;j++)
             for(int i=x1+1;i<=x2;i++)
-                arr[i][j]++;
+                arr[i+1000][j+1000]++;
     }
     cin>>x1>>y1>>x2>>y2;
     for(int j=y1+1;j<=y2;j++)
         for(int i=x1+1;i<=x2;i++)
-            arr[i][j]=0;
+            arr[i+1000][j+1000]=0;
     
     int sum = 0;
     for(int i=0;i<2001;i++)
         for(int j=0;j<2001;j++)
-            if(arr[i][j]==1) sum++;
+            if(arr[i+1000][j+1000]==1) sum++;
     cout<<sum;
     return 0;
 }
