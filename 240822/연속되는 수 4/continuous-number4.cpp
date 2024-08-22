7391753da3941ea4a51515d80d7a34883c2718a7
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-08-22 09:35:36 +0900

    Title: 연속되는 수 4, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240822/연속되는 수 4/continuous-number4.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n,max=0;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++)
+        cin>>arr[i];
+    for(int i=0,cnt=0;i<n;i++)
+    {
+        if(i==0 || arr[i-1]>=arr[i]) cnt = 0;
+        cnt++;
+        if(cnt > max) max = cnt; 
+    }
+    cout<<max;
+    return 0;
+}
