a0be16a7080296389fd11d9a53d7db1e5eb004df
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-08-22 09:27:49 +0900

    Title: 연속되는 수 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240822/연속되는 수 2/continuous-number2.cpp
+++ 240822/연속되는 수 2/continuous-number2.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 using namespace std;
-
 int main() {
     int n,max=0;
     cin>>n;
@@ -9,10 +8,7 @@ int main() {
         cin>>arr[i];
     for(int i=0,cnt=0;i<n;i++)
     {
-        if(i==0 || arr[i] != arr[i-1])
-        {
-            cnt=0;
-        }
+        if(i==0 || arr[i] != arr[i-1])cnt=0;
         cnt++;
         if(max < cnt) max = cnt;
     }
