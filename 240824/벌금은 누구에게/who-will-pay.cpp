0d6336b9023e048662c2cde30bd7bbbb4b4269eb
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-08-24 23:43:45 +0900

    Title: 벌금은 누구에게, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 240824/벌금은 누구에게/who-will-pay.cpp
+++ 240824/벌금은 누구에게/who-will-pay.cpp
@@ -1,22 +1,33 @@
 #include <iostream>
+
+#define MAX_N 100
+#define MAX_M 10000
+
 using namespace std;
+
+int n, m, k;
+int penalized_person[MAX_M];
+int penalty_num[MAX_N + 1];
+
 int main() {
-    int n,m,k;
-    int arr[n+1] = {0,};
-    int save[m] = {0,};
-    cin>>n>>m>>k;
-    for(int i=0;i<m;i++)
-        cin>>save[i];
-    for(int i=0;i<m;i++)
-    {
-        int target = save[i];
-        arr[target]++;
-        if(arr[target] >= k)
-        {
-            cout<<target;
-            return 0;
-        } 
+    // 입력
+    cin >> n >> m >> k;
+    for(int i = 0; i < m; i++)
+        cin >> penalized_person[i];
+    
+    // 각 패널티 횟수를 세서,
+    // 최초로 K번 이상 벌칙을 받는 사람을 추적합니다.
+    int ans = -1;
+    for(int i = 0; i < m; i++) {
+        int target = penalized_person[i];
+        penalty_num[target]++;
+        
+        if(penalty_num[target] >= k) {
+            ans = target;
+            break;
+        }
     }
-    cout<<-1;
+    
+    cout << ans;
     return 0;
 }
