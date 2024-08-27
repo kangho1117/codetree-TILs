b3b488fd278225868f3a602fa906c79ab5491b91
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-08-27 09:34:36 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240827/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.cpp
+++ 240827/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.cpp
@@ -18,9 +18,6 @@ int main() {
     ans[P] = {1,K};
     for(auto &a : infect)
     {
-        // for(int i=1; i<=N; i++)
-        // cout<<"{"<<ans[i].first<<" "<<ans[i].second<<"}";
-        // cout<<"\n\n";
         if(ans[get<1>(a)].first == 1 && ans[get<1>(a)].second > 0 && ans[get<2>(a)].first == 1 && ans[get<2>(a)].second > 0 )
         {
             ans[get<1>(a)].second--;
@@ -46,12 +43,7 @@ int main() {
         }
 
     }
-    // for(int i=1; i<=N; i++)
-    //     cout<<"{"<<ans[i].first<<" "<<ans[i].second<<"}";
-    //     cout<<"\n\n";
-    // for(auto &a : infect)
-    //     cout<<get<0>(a)<<" "<<get<1>(a)<<" "<<get<2>(a)<<"\n";
-        for(int i=1; i<=N; i++)
-        cout<<ans[i].first;
+    for(int i=1; i<=N; i++)
+    cout<<ans[i].first;
     return 0;
 }
