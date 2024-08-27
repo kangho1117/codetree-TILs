2890cf598fa9e9dc67cadb2a257b1c31da29bb0a
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-08-27 09:18:55 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240827/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <tuple>
+#include <algorithm>
+using namespace std;
+int main() {
+    int N,K,P,T;
+    cin>>N>>K>>P>>T;
+    vector<tuple<int,int,int>> infect;
+    vector<pair<int,int>> ans(N+1,{0,0});
+    for(int i=0;i<T;i++)
+    {
+        int t,x,y;
+        cin>>t>>x>>y;
+        infect.push_back(make_tuple(t,x,y));
+    }
+    sort(infect.begin(), infect.end());
+    ans[P] = {1,K};
+    for(auto &a : infect)
+    {
+        if(ans[get<1>(a)].first == 1 && ans[get<1>(a)].second > 0 )
+        {
+            ans[get<1>(a)].second -= 1;
+            if(ans[get<2>(a)].first == 0)
+            {
+                ans[get<2>(a)].first = 1;
+                ans[get<2>(a)].second = K;
+            }
+        }
+    }
+    for(int i=1; i<=N; i++)
+        cout<<ans[i].first;
+    return 0;
+}
