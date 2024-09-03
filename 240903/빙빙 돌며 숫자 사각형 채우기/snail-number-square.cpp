2ef40714f96bd983890cb9a268c8d31a3d4d8ce6
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-09-03 09:30:38 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240903/빙빙 돌며 숫자 사각형 채우기/snail-number-square.cpp
+++ 240903/빙빙 돌며 숫자 사각형 채우기/snail-number-square.cpp
@@ -9,22 +9,19 @@ int main() {
         if(x==1 && y==1) arr[x][y] = 1;
         else arr[x][y] = arr[x-dx[d]][y-dy[d]] + 1;
 
-        if(n<(x + dx[d])) {d=(d+1)%4;}
-        else if((x + dx[d]) < 1) {d=(d+1)%4;}
-        else if(m<(y + dy[d])) {d=(d+1)%4;}
-        else if((y + dy[d]) < 1) {d=(d+1)%4;}
-        else if(arr[x + dx[d]][y + dy[d]] != 0) {d=(d+1)%4;}
+        if(n<(x + dx[d])) d=(d+1)%4;
+        else if((x + dx[d]) < 1) d=(d+1)%4;
+        else if(m<(y + dy[d])) d=(d+1)%4;
+        else if((y + dy[d]) < 1) d=(d+1)%4;
+        else if(arr[x + dx[d]][y + dy[d]] != 0) d=(d+1)%4;
         x += dx[d];
         y += dy[d];
     }
         for(int i=1;i<=n;i++)
         {
-        for(int j=1;j<=m;j++)
-        {
-            cout<<arr[i][j]<<" ";
+            for(int j=1;j<=m;j++)
+                cout<<arr[i][j]<<" ";
+            cout<<"\n";
         }
-        cout<<"\n";
-        }
-        cout<<"\n\n";
     return 0;
 }
