6a183e862b4eed737cd45075547a315cee6677e4
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-09-04 09:01:19 +0900

    Title: 되돌아오기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240904/되돌아오기/come-back.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n,x=0,y=0,t=0;
+    cin>>n;
+    while(n--)
+    {
+        char a;
+        int b;
+        cin>>a>>b;
+        if(a=='E'){
+            while(b--)
+            {
+                
+                y++;
+                t++;
+                //cout<<x<<" "<<y<<" "<<t<<"\n";
+                if(x==0 && y==0)
+                {
+                    cout<<t;
+                    return 0;
+                }
+            }
+        }
+        else if(a=='S'){
+            while(b--)
+            {
+                
+                x++;
+                t++;
+                //cout<<x<<" "<<y<<" "<<t<<"\n";
+                if(x==0 && y==0)
+                {
+                    cout<<t;
+                    return 0;
+                }
+            }
+        }
+        else if(a=='W'){
+            while(b--)
+            {
+                y--;
+                t++;
+                //cout<<x<<" "<<y<<" "<<t<<"\n";
+                if(x==0 && y==0)
+                {
+                    cout<<t;
+                    return 0;
+                }
+            }
+        }
+        else if(a=='N'){
+            while(b--)
+            {
+                x--;
+                t++;
+                //cout<<x<<" "<<y<<" "<<t<<"\n";
+                if(x==0 && y==0)
+                {
+                    cout<<t;
+                    return 0;
+                }
+            }
+        }
+    }
+    cout<<-1;
+    return 0;
+}
