1fdde5279d03bf4127df770dfbef0a17ea9a1afe
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-09-08 13:45:40 +0900

    Title: 거울에 레이저 쏘기 2, Time: 62ms, Memory: 0MB, Status: Passed - CodeTree

--- 240908/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.cpp
+++ 240908/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 using namespace std;
 int main() {
-    int k,n,x=1,y=1,dx[4] = {0,1,0,-1}, dy[4] = {1,0,-1,0};
+    int k,n,x=1,y=1,dx[4] = {0,1,0,-1}, dy[4] = {1,0,-1,0},ting=0;;
     cin>>n;
     vector<vector<char>> vec(n+2, vector<char>(n+2,'x'));
     for(int i=1;i<=n;i++)
@@ -23,15 +23,10 @@ int main() {
         y += dy[d];
         temp++;
     }
-
-    
-
     if(temp<n) d = 1;
     else if(temp<2*n) d=2;
     else if(temp<3*n) d=3;
     else d=0;
-
-    int ting=0;
     while(1)
     {
         if(vec[x][y] == 'x') break;
