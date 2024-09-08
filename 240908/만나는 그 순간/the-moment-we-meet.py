858baa47f0604efe9c9a1330fe9269cfb5dd2a08
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-09-08 14:38:58 +0900

    Title: 만나는 그 순간, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240908/만나는 그 순간/the-moment-we-meet.py
+++ 240908/만나는 그 순간/the-moment-we-meet.py
@@ -1,35 +1,26 @@
+MAX_T = 1000000
+
 n, m = map(int, input().split())
-A_lst = []
-A_cnt = 0
-B_lst = []
-B_cnt = 0
+
+pos_a, pos_b = [0] * (MAX_T + 1), [0] * (MAX_T + 1)
+
+time_a = 1
 for _ in range(n):
-    dir, dis = input().split()
-    dis = int(dis)
-    if dir == 'L':
-        for j in range(dis):
-            A_cnt -= 1
-            A_lst.append(A_cnt)
-    else :
-        for j in range(dis):
-            A_cnt += 1
-            A_lst.append(A_cnt)
-
+    d, t = input().split()
+    for _ in range(int(t)):
+        pos_a[time_a] = pos_a[time_a] + 1 (1 if d == 'R' else -1)
+        time_a += 1
+time_b = 1
 for _ in range(m):
-    dir, dis = input().split()
-    dis = int(dis)
-    if dir == 'L':
-        for j in range(dis):
-            B_cnt -= 1
-            B_lst.append(B_cnt)
-    else :
-        for j in range(dis):
-            B_cnt += 1
-            B_lst.append(B_cnt)
+    d, t = input().split()
+    for _ in range(int(t)):
+        pos_b[time_b] = pos_b[time_b] + 1 (1 if d == 'R' else -1)
+        time_b += 1
 
-for i in range(len(A_lst)):
-    if A_lst[i] == B_lst[i]:
-        print(i+1)
+ans = -1
+
+for i in range(1, time_a):
+    if pos_a[i] == pos_b[i]:
+        ans = i
         break
-    if i == len(A_lst) - 1:
-        print(-1)
+print(ans)
