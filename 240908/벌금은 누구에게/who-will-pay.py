5d51bd6f02e86280d20cc5835ca759fc94831064
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-09-08 14:57:30 +0900

    Title: 벌금은 누구에게, Time: 137ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240908/벌금은 누구에게/who-will-pay.py
@@ -0,0 +1,12 @@
+n, m, k = map(int, input().split())
+
+stu_arr = [0] * (n+1)
+
+ans = -1
+for i in range(m):
+    num = int(input())
+    stu_arr[num] += 1
+    if stu_arr[num] >= k:
+        ans = num
+
+print(ans)
