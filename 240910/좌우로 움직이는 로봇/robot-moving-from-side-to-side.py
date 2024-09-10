234fe633ab2d6a7c22469bada99681d42a99caec
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-09-10 23:05:03 +0900

    Title: 좌우로 움직이는 로봇, Time: 142ms, Memory: 40MB, Status: Wrong Answer - CodeTree

--- 240910/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
+++ 240910/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
@@ -21,20 +21,20 @@ for _ in range(m):
         pos_b[time_b] = pos_b[time_b - 1] + (1 if d == 'R' else -1)
         time_b += 1
 
-if time_a > time_b:
-    for i in range(time_a + 1, time_b+1):
+if time_a < time_b:
+    for i in range(time_a, time_b):
         pos_a[i] = pos_a[time_a]
 if time_b > time_a:
-    for i in range(time_b + 1, time_a + 1):
+    for i in range(time_b, time_a):
         pos_b[i] = pos_b[time_b]
 
 cnt = 0
 #print(pos_a[14158],pos_b[14158],pos_a[14159],pos_b[14159])
-for i in range(2, len(pos_a)):
+for i in range(1, len(pos_a)):
     #print(i, pos_a[i], pos_b[i])
-    if pos_b[i] == 0 and pos_a[i] == 0:
+    if i == max(time_a, time_b) + 1:
         break
     if pos_a[i] == pos_b[i] and pos_a[i-1] != pos_b[i-1]:
         cnt += 1
-        #print(i, pos_a[i], pos_b[i],cnt)
+        #print(cnt)
 print(cnt)
