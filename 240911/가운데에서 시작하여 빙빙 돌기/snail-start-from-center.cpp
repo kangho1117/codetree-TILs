72a847301fa524ee14894bb3804514c0e7b67185
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-09-11 11:06:23 +0900

    Title: 가운데에서 시작하여 빙빙 돌기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240911/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.cpp
+++ 240911/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.cpp
@@ -10,28 +10,13 @@ int main() {
     while(num <= n*n)
     {
         num++;
-        //cout<<x + dx[d]<<" "<<y+dy[d]<<"\n";
-        //cout<<arr[x + dx[d]][y+dy[d]]<<"\n";
-        if(arr[x + dx[d]][y+dy[d]] == 0 && 0 < x + dx[d] && x + dx[d] <=n && 0 < y+dy[d] && y+dy[d] <=n)
+        if(arr[x + dx[d]][y+dy[d]] == 0)
         {
             x += dx[d];
             y += dy[d];
             arr[x][y] = num;
             if(arr[x + dx[(d-1+4)%4]][y+dy[(d-1+4)%4]] == 0) d = (d-1+4)%4;
         }
-
-
-        
-    // for(int i=1;i<=n;i++)
-    // {
-    //     for(int j=1;j<=n;j++)
-    //     {
-    //         cout<<arr[i][j]<<" ";
-    //     }
-    //     cout<<"\n";
-    // }
-    // cout<<d<<" \n\n";
-    
     }
     for(int i=1;i<=n;i++)
     {
