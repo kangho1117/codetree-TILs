1d1544b1de33f6c68ae3f31162864184641c436a
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-09-11 20:45:37 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 120ms, Memory: 24MB, Status: Passed - CodeTree

--- 240911/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
+++ 240911/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -1,31 +1,23 @@
-MAX_T = 250
 N, K, P, T = map(int, input().split())
 
-t_arr = []
-devel_2d = [[0 for _ in range(MAX_T + 1)] for _ in range(N+1)]
-
-dev_cnt = [0] * (N+1)
-for i in range(1, MAX_T+1):
-    devel_2d[P][i] = 1
+hand_cnt = [K for _ in range(N+1)]
+infect = [0 for _ in range(N+1)]
+infect[P] = 1
 
 arr_2d = [list(map(int, input().split())) for _ in range(T)]
-
 
 arr_2d.sort(key = lambda x:x[0])
 
 for t,x,y in arr_2d:
-    t_arr.append(t)
-    if devel_2d[x][t] == 1 and dev_cnt[x] < K:
-        #print(t, x, y)
-        for i in range(t,MAX_T):
-            devel_2d[y][i] = 1
-        dev_cnt[x] += 1
-    elif devel_2d[y][t] == 1 and dev_cnt[y] < K:
-        #print(t, x, y)
-        for i in range(t,MAX_T):
-            devel_2d[x][i] = 1
-        dev_cnt[y] += 1
-        
-  
+    if (infect[x] == 0 and infect[y] == 1) and hand_cnt[y] > 0:
+        infect[x] = 1
+        hand_cnt[y] -= 1
+    elif (infect[x] == 1 and infect[y] == 0) and hand_cnt[x] > 0:
+        infect[y] = 1
+        hand_cnt[x] -= 1
+    elif (infect[x] == 1 and infect[y] == 1) and (hand_cnt[x] > 0 or hand_cnt[y] > 0):
+        hand_cnt[x] -= 1
+        hand_cnt[y] -= 1
+
 for i in range(1,N+1):
-    print(devel_2d[i][max(t_arr)], end = '')
+    print(infect[i], end = '')
