fcfc1dcb953fc8a0ad52a9972b2ffe7c5a900d4f
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-09-13 09:44:41 +0900

    Title: 모이자, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240913/모이자/gather.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    int arr[n],sum=0,min=999;
+    for(int i=0;i<n;i++)
+        cin>>arr[i];
+    
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            if(i>j) sum += arr[j] * (i-j);
+            else if(i<=j) sum += arr[j] * (j-i);
+        }
+        if(sum < min) min = sum;
+        sum = 0;
+    }
+    cout<<min;
+    return 0;
+}
