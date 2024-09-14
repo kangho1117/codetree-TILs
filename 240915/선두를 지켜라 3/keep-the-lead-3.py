30929c98ce3f412ca09374280aff7de5717a72a7
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-09-15 00:28:38 +0900

    Title: 선두를 지켜라 3, Time: 131ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240915/선두를 지켜라 3/keep-the-lead-3.py
@@ -0,0 +1,43 @@
+Max_T = 1000
+N, M = map(int, input().split())
+
+pos_a, pos_b = [0] * (Max_T + 1 ),[0] * (Max_T + 1 ) 
+
+time_a = 1
+for _ in range(N):
+    v, t = map(int, input().split())
+
+    for _ in range(t):
+        pos_a[time_a] = pos_a[time_a - 1] + v
+        time_a += 1
+
+time_b = 1
+for _ in range(M):
+    v, t = map(int, input().split())
+
+    for _ in range(t):
+        pos_b[time_b] = pos_b[time_b - 1] + v
+        time_b += 1
+
+
+leader, answer = 0, 0
+for i in range(1, time_a):
+    print(i, pos_a[i], pos_b[i])
+    if pos_a[i] > pos_b[i]:
+
+        if leader == 2 or leader == 0:
+            answer += 1
+            #print(i, pos_a[i], pos_b[i], answer)
+        leader = 1
+    elif pos_a[i] < pos_b[i]:
+
+        if leader == 1 or leader == 0:
+            answer += 1
+            #print(i, pos_a[i], pos_b[i], answer)
+        leader = 2
+    else :
+        if leader == 1 or leader == 2:
+            answer += 1
+            #print(i, pos_a[i], pos_b[i], answer)
+        leader = 0
+print(answer)
