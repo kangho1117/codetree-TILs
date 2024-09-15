ccae02a93e8ded8234bd557ae3668da4fdf082c5
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-09-15 11:46:49 +0900

    Title: 일렬로 서있는 소 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240915/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.cpp
+++ 240915/일렬로 서있는 소 2/cattle-in-a-rowing-up-2.cpp
@@ -7,15 +7,9 @@ int main() {
     for(int i=0;i<n;i++)
         cin>>arr[i];
     for(int i=0;i<n;i++)
-    {
         for(int j=i+1;j<n;j++)
-        {
             for(int k=j+1;k<n;k++)
-            {
                 if(arr[i] <= arr[j] && arr[j] <= arr[k]) sum++;
-            }
-        }
-    }
     cout<<sum;
     return 0;
 }
