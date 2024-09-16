33de73ab9015cf7f1a398e940bd64c81187e3f56
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-09-16 14:58:35 +0900

    Title: 최고의 13위치, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240916/최고의 13위치/best-place-of-13.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main() {
+    int n,max=0;
+    cin>>n;
+    vector<vector<int>> vec(n+2,vector<int>(n+2,0));
+    for(int i=1;i<=n;i++)
+        for(int j=1;j<=n;j++)
+            cin>>vec[i][j];
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1,temp=0;j<=n-2;j++)
+        {
+            if(vec[i][j]) temp++;
+            if(vec[i][j+1]) temp++;
+            if(vec[i][j+2]) temp++;
+            if(max < temp) max = temp;
+        }
+    }
+    cout<<max;
+    return 0;
+}
