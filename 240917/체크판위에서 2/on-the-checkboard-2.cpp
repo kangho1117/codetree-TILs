78280037e4d45c18eef093a5ad1e4d827b6ebc5b
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-09-17 09:54:12 +0900

    Title: 체크판위에서 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240917/체크판위에서 2/on-the-checkboard-2.cpp
+++ 240917/체크판위에서 2/on-the-checkboard-2.cpp
@@ -9,13 +9,13 @@ int main() {
         for(int j=1;j<=c;j++)
             cin>>vec[i][j];
 
-    for(int i=2;i<r;i++)
+    for(int i=2;i<=r;i++)
     {
-        for(int j=2;j<c;j++)
+        for(int j=2;j<=c;j++)
         {
-            for(int k=i+1;k<r-1;k++)
+            for(int k=i+1;k<r;k++)
             {
-                for(int l=j+1;l<c-1;l++)
+                for(int l=j+1;l<c;l++)
                 {
                     if(vec[1][1] != vec[i][j] &&
                     vec[i][j] != vec[k][l] &&
