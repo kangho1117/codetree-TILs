d682f465593c3d6b49bb8646dcf694d8894c3786
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-09-19 09:24:42 +0900

    Title: 이상한 진수 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240919/이상한 진수 2/awkward-digits-2.cpp
+++ 240919/이상한 진수 2/awkward-digits-2.cpp
@@ -5,14 +5,17 @@ using namespace std;
 int main() {
     string s;
     cin>>s;
+    bool all=true;
     for(auto &a : s)
     {
         if(a=='0')
         {
             a='1';
+            all = false;
             break;
         }
     }
+    if(all) s.back() = '0';
     int sum=0;
     for(int i=s.size()-1,j=0;i>=0;i--,j++)
         sum += (s[i]-48)*pow(2,j);
