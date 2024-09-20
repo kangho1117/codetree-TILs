d179e575e0df67d8a39de76599d98bce67c01ebb
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-09-21 01:01:14 +0900

    Title: 원 모양으로 되어있는 방, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240921/원 모양으로 되어있는 방/a-room-in-a-circle.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <list>
+using namespace std;
+int main() {
+    int n,min=100300;
+    cin>>n;
+    list<int> l;
+    for(int i=0,temp;i<n;i++)
+    {
+        cin>>temp;
+        l.push_back(temp);
+    }
+    for(int i=0;i<n;i++)
+    {
+        int sum=0;
+        for(int j=0;j<n;j++)
+        {
+            sum += j * l.front();
+            l.push_back(l.front());
+            l.pop_front();
+        }
+        if(sum < min) min = sum;
+        l.push_back(l.front());
+        l.pop_front();
+    }
+    cout<<min;
+    return 0;
+}
