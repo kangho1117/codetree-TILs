0461a5496496cb515ecb599e7c66e900a784505c
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-09-23 10:24:24 +0900

    Title: 괄호 쌍 만들어주기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240923/괄호 쌍 만들어주기 2/pair-parentheses-2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    string str;
+    cin>>str;
+    int cnt=0;
+    for(int i=0;i<str.size()-3;i++)
+    {
+        if(str[i]=='(' && str[i+1] == '(')
+        {
+            for(int j = i+2;j<str.size()-1;j++)
+            {
+                if(str[j]==')' && str[j+1] == ')') cnt++;
+            }
+        }
+    }
+    cout<<cnt;
+    return 0;
+}
