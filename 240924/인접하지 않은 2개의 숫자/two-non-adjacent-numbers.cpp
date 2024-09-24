a0e917075e2085a320307d8cc4101ce8211c1cde
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-09-24 09:20:44 +0900

    Title: 인접하지 않은 2개의 숫자, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240924/인접하지 않은 2개의 숫자/two-non-adjacent-numbers.cpp
+++ 240924/인접하지 않은 2개의 숫자/two-non-adjacent-numbers.cpp
@@ -7,12 +7,8 @@ int main() {
     for(int i=0;i<n;i++)
         cin>>arr[i];
     for(int i=0;i<n-2;i++)
-    {
         for(int j=i+2;j<n;j++)
-        {
             if(arr[i] + arr[j] > max) max = arr[i] + arr[j];
-        }
-    }
     cout<<max;
     return 0;
 }
