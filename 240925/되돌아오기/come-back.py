fc070a0a8e81e91f38736497e27a715fe27838b0
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-09-25 18:29:27 +0900

    Title: 되돌아오기, Time: 141ms, Memory: 24MB, Status: Passed - CodeTree

--- 240925/되돌아오기/come-back.py
+++ 240925/되돌아오기/come-back.py
@@ -8,13 +8,18 @@ dx,dy = [1,0,-1,0],[0,-1,0,1]
 answer = -1
 t = 0
 for _ in range(n):
+    if answer != -1:
+        break
     dir, dist = input().split()
     move_dir = dir_dict[dir]
     for i in range(int(dist)):
-        x,y = x + dx[move_dir], y + dy[move_dir]
         t += 1
+        x,y = x + dx[move_dir], y + dy[move_dir]
+        
+
         if  x == 0 and y == 0:
             answer = t
+            break
         
     
 print(answer)
