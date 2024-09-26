be14ceca9535d9444e2952478572294dcf073b6f
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-09-26 10:12:41 +0900

    Title: Carry 피하기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240926/Carry 피하기 2/escaping-carry-2.cpp
+++ 240926/Carry 피하기 2/escaping-carry-2.cpp
@@ -12,27 +12,14 @@ int main() {
         {
             for(int k=j+1;k<n;k++)
             {
-                int temp1 = 0,temp10 = 0,temp100 = 0,temp1000 = 0, temp10000 = 0;
-                if(arr[i]%10 + arr[j]%10 + arr[k]%10 < 10)
+                if(arr[i]%10 + arr[j]%10 + arr[k]%10 < 10
+                    && arr[i]%100/10 + arr[j]%100/10 + arr[k]%100/10 < 10
+                    && arr[i]%1000/100 + arr[j]%1000/100 + arr[k]%1000/100 < 10
+                    && arr[i]%10000/1000 + arr[j]%10000/1000 + arr[k]%10000/1000 < 10
+                    && arr[i]/10000 + arr[j]/10000 + arr[k]/10000 < 10)
                 {
-                    temp1 = (arr[i]%10 + arr[j]%10 + arr[k]%10);
-                    if(arr[i]%100/10 + arr[j]%100/10 + arr[k]%100/10 < 10)
-                    {
-                        temp10 = (arr[i]%100/10 + arr[j]%100/10 + arr[k]%100/10);
-                        if(arr[i]%1000/100 + arr[j]%1000/100 + arr[k]%1000/100 < 10)
-                        {
-                            temp100 = (arr[i]%1000/100 + arr[j]%1000/100 + arr[k]%1000/100);
-                            if(arr[i]%10000/1000 + arr[j]%10000/1000 + arr[k]%10000/1000 < 10)
-                            {
-                                temp1000 = (arr[i]%10000/1000 + arr[j]%10000/1000 + arr[k]%10000/1000);
-                                if(arr[i]/10000 + arr[j]/10000 + arr[k]/10000 < 10)
-                                {
-                                    temp10000 = arr[i]/10000 + arr[j]/10000 + arr[k]/10000;
-                                    if(max < temp10000*10000 + temp1000*1000 + temp100*100 + temp10*10 + temp1) max = temp10000*10000 + temp1000*1000 + temp100*100 + temp10*10 + temp1;
-                                }
-                            }
-                        }
-                    }
+                    int temp = (arr[i]%10 + arr[j]%10 + arr[k]%10) + (arr[i]%100/10 + arr[j]%100/10 + arr[k]%100/10)*10 + (arr[i]%1000/100 + arr[j]%1000/100 + arr[k]%1000/100)*100 + (arr[i]%10000/1000 + arr[j]%10000/1000 + arr[k]%10000/1000)*1000 + (arr[i]/10000 + arr[j]/10000 + arr[k]/10000)*10000;
+                    if(max < temp) max = temp;   
                 }
             }
         }
