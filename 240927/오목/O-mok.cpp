394ca43bc54aa3dfaae884735646fea72edcb15a
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-09-27 10:08:11 +0900

    Title: 오목, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240927/오목/O-mok.cpp
+++ 240927/오목/O-mok.cpp
@@ -106,6 +106,40 @@ int main() {
             }
         }
     }
+
+    for(int i=1; i<=19; i++)
+    {
+        for(int j=i,k=1,b=0,w=0; j>=1 || k<=19; j--,k++)
+        {
+            if(vec[k][j] == 1)
+             {
+                b++;
+                w=0;
+            }
+            else if(vec[k][j] == 2)
+             {
+                w++;
+                b=0;
+            }
+            else
+            {
+                w=0;
+                b=0;
+            }
+            if(b==5)
+            {
+                cout<<"1\n";
+                cout<<k-2<<" "<<j+2;
+                return 0;
+            }
+            else if(w==5)
+            {
+                cout<<"2\n";
+                cout<<k-2<<" "<<j+2;
+                return 0;
+            }
+        }
+    }
     cout<<0;
     return 0;
 }
