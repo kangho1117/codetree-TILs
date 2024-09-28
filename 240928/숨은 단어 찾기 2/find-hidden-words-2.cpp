52e41767f53ff360817196fa3c23a6bb7423ba21
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-09-28 17:50:40 +0900

    Title: 숨은 단어 찾기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240928/숨은 단어 찾기 2/find-hidden-words-2.cpp
+++ 240928/숨은 단어 찾기 2/find-hidden-words-2.cpp
@@ -10,10 +10,7 @@ int main() {
     {
         string str;
         cin>>str;
-        for(int j=0;j<m;j++)
-        {
-            vec[i][j] = str[j];
-        }
+        for(int j=0;j<m;j++) vec[i][j] = str[j];
     }
     for(int i=0;i<n;i++)
     {
@@ -21,21 +18,13 @@ int main() {
         {
             if(vec[i][j]=='L')
             {
-                //우
                 if(j+2 < m && vec[i][j+1] =='E' && vec[i][j+2] == 'E') cnt++;
-                //좌
                 if(0 <= j-2 && vec[i][j-1] =='E' && vec[i][j-2] == 'E') cnt++;
-                //상
                 if(0 <= i-2 && vec[i-1][j] =='E' && vec[i-2][j] == 'E') cnt++;
-                //하
                 if(i+2 < n && vec[i+1][j] =='E' && vec[i+2][j] == 'E') cnt++;
-                //우상
                 if(0 <= i-2 && j+2 < m && vec[i-1][j+1] =='E' && vec[i-2][j+2] == 'E') cnt++;
-                //우하
                 if(i+2 < n && j+2 < m && vec[i+1][j+1] =='E' && vec[i+2][j+2] == 'E') cnt++;
-                //좌상
                 if(0 <= i-2 && 0 <= j-2 && vec[i-1][j-1] =='E' && vec[i-2][j-2] == 'E') cnt++;
-                //좌하
                 if(i+2 < n && 0 <= j-2 && vec[i+1][j-1] =='E' && vec[i+2][j-2] == 'E') cnt++;
             }
         }
