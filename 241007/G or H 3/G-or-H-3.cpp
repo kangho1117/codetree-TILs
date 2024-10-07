422826d600a9c1474fd4766a43a1853a0ffd89d0
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-10-07 10:03:50 +0900

    Title: G or H 3, Time: 72ms, Memory: 0MB, Status: Passed - CodeTree

--- 241007/G or H 3/G-or-H-3.cpp
+++ 241007/G or H 3/G-or-H-3.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 int main() {
-    int n,k,max=0,ans=0;
+    int n,k,ans=0;
     char arr[10002]={0,}; 
     cin>>n>>k;
     for(int i=0;i<n;i++)
@@ -10,12 +10,11 @@ int main() {
         int t;
         cin>>t>>c;
         arr[t] = c;
-        if(t > max) max = t;
     }
-    for(int i=1,temp;i<max-k+1;i++)
+    for(int i=1,temp;i<=10000;i++)
     {
         temp=0;
-        for(int j=i;j<=i+k;j++)
+        for(int j=i;j<=i+k && j<=10000;j++)
         {
             if(arr[j]=='G') temp+=1;
             else if(arr[j]=='H') temp+=2;
