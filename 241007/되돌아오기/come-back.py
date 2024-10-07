599063df940c7f574feff360c6c00bf4813bccf8
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-10-07 11:38:46 +0900

    Title: 되돌아오기, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241007/되돌아오기/come-back.py
@@ -0,0 +1,26 @@
+x, y = 0, 0
+
+n = int(input())
+
+direction_dict = {
+    'E':0,
+    'S':1,
+    'W':2,
+    'N':3
+}
+
+dx, dy = [1, 0 ,-1, 0], [0, -1, 0, 1]
+
+answer = 0
+time = 0
+
+for _ in range(n):
+    dir_C, distance = input().split()
+    dir = direction_dict[dir_C]
+    distance = int(distance)
+    for _ in range(distance):
+        time += 1
+        x,y = x + dx[dir], y + dy[dir]
+        if x == 0 and y == 0:
+            answer = time
+print(answer)
