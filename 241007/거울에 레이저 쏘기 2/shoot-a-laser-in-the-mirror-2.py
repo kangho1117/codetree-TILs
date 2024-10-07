294060104a065f9de69bc398062ac041fb79fb39
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-10-07 16:48:31 +0900

    Title: 거울에 레이저 쏘기 2, Time: 144ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241007/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.py
@@ -0,0 +1,37 @@
+# / -> 반시계 90
+# \ -> 시계 90
+
+n = int(input())
+
+arr_2d = [input() for _ in range(n)]
+
+dxs, dys = [1, 0, -1, 0], [0, -1, 0 ,1]
+
+k = int(input())
+
+def initialize(num):
+    if num <= n:
+        return 0, num - 1, 0
+    elif num <= 2 * n :
+        return num - n - 1, n - 1, 1
+    elif num <= 3 * n:
+        return n - 1, n - (num - 2 * n), 2
+    else :
+        return n - (num - 3 * n), 0, 3
+
+def in_range(x, y):
+    return 0 <= x and x < n and 0 <= y and y < n
+
+x, y, move_dir = initialize(k)
+
+move_dir = 0
+move_num = 0
+while in_range(x,y):
+    if arr_2d[x][y] == '/':
+        move_dir = move_dir ^ 1
+        x, y = x + dxs[move_dir], y + dys[move_dir]
+    else:
+        move_dir = 3 - move_dir
+        x, y = x + dxs[move_dir], y + dys[move_dir]   
+    move_num += 1
+print(move_num)
