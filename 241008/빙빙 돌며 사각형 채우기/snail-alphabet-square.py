919fa46346e5cbe95ee432ed9f1e6d4e092204a9
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-10-08 14:52:29 +0900

    Title: 빙빙 돌며 사각형 채우기, Time: 278ms, Memory: 27MB, Status: Wrong Answer - CodeTree

--- 241008/빙빙 돌며 사각형 채우기/snail-alphabet-square.py
+++ 241008/빙빙 돌며 사각형 채우기/snail-alphabet-square.py
@@ -11,22 +11,22 @@ def in_range(x,y):
     return 0 <= x and x < n and 0<=y and y < m
 
 x,y = 0,0
-j = 0
+j = 1
 dir_num = 0
-arr_2d[x][y] = alphabet_list[j]
+arr_2d[x][y] = alphabet_list[0]
 for i in range(1, n*m):
     dx, dy = x + dxs[dir_num], y + dys[dir_num]
     if not in_range(dx, dy) or arr_2d[dx][dy] != 0:
         dir_num = (dir_num + 1) % 4
-    j += 1
+    if i % 24 == 0:
+        j = 0
 
     x, y = x + dxs[dir_num], y + dys[dir_num]
     arr_2d[x][y] = alphabet_list[j]
     #print(j,x,y,arr_2d[x][y])
-    
+    j += 1
 
-    if i % 24 == 0:
-        j = 0
+    
 
 for arr in arr_2d:
     for elem in arr:
