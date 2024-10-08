288019d02d0481871a0ace3411d8b4272539b86c
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-10-08 10:51:35 +0900

    Title: 특정 구간의 원소 평균값, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241008/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.cpp
+++ 241008/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.cpp
@@ -8,28 +8,23 @@ int main() {
         cin>>arr[i];
     for(int k=1; k<=n; k++)
     {
-        for(int i = 0, sum; i<n; i++)
+        for(int i = 0; i<n; i++)
         {
-            sum=0;
-            for(int j=i; i+k<=n && j<i+k ; j++)
+            double sum=0;
+            for(int j=i; i+k<=n && j<i+k ; j++) sum+=arr[j];
+            sum/=(double)k;
+            if(sum - (int)sum == 0)
             {
-                sum+=arr[j];
-            }
-            double temp = (double)sum/(double)k;
-            if(temp - (int)temp == 0)
-            {
-            for(int j=i; i+k<=n && j<i+k ; j++)
-            {
-                if(sum==0) break;
-                else if(arr[j] == temp)
+                for(int j=i; i+k<=n && j<i+k ; j++)
                 {
-                    cnt++;
-                    break;
+                    if(sum==0) break;
+                    else if(arr[j] == sum)
+                    {
+                        cnt++;
+                        break;
+                    }
                 }
             }
-                
-            }
-            //cout<<k<<" "<<temp<<" "<<i<<" "<<i+k-1<<"\n";
         }
     }
     cout<<cnt;
