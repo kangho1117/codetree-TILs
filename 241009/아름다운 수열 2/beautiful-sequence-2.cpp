a390fb2e75a7fa19425bdca56c4d96f609a02fb7
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-10-09 13:35:29 +0900

    Title: 아름다운 수열 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241009/아름다운 수열 2/beautiful-sequence-2.cpp
+++ 241009/아름다운 수열 2/beautiful-sequence-2.cpp
@@ -10,7 +10,7 @@ int main() {
         cin>>A[i];
     for(int i=0;i<n;i++)
         cin>>B[i];
-    sort(B.begin(), B.end());
+    //sort(B.begin(), B.end());
     do
     {
         auto it = A.begin();
