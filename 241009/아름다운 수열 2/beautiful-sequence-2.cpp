e7fba30f2058df5b09ce1694db6cef676139ef92
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-10-09 14:08:34 +0900

    Title: 아름다운 수열 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241009/아름다운 수열 2/beautiful-sequence-2.cpp
+++ 241009/아름다운 수열 2/beautiful-sequence-2.cpp
@@ -39,7 +39,7 @@ int main() {
             bool err = false;
             for(int i=0;i<m;i++,temp++)
             {
-                //cout<<B[i]<<" "<<A[temp-A.begin()]<<" "<<temp-A.begin()<<"\n";
+                cout<<B[i]<<" "<<A[temp-A.begin()]<<" "<<temp-A.begin()<<"\n";
                 if(B.front() != A[temp-A.begin()]) it++;
                 if(B[i] != A[temp-A.begin()])
                 {
@@ -47,7 +47,7 @@ int main() {
                     break;
                 }
             }
-            //cout<<"\n\n";
+            cout<<"\n\n";
             it++;
             if(!err) cnt++;
             else if(temp == A.end()) break;
