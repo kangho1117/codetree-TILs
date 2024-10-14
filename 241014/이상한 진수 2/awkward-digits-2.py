7bcda29bda02b83c0060c6bc9214f93e2deaccca
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-10-14 17:08:52 +0900

    Title: 이상한 진수 2, Time: 110ms, Memory: 24MB, Status: Passed - CodeTree

--- 241014/이상한 진수 2/awkward-digits-2.py
+++ 241014/이상한 진수 2/awkward-digits-2.py
@@ -15,11 +15,13 @@ for i in range(n):
         arr[i] = 0
 
 answer = 0
+#print(arr[-7])
 for i in range(n-1,-1,-1):
-    if arr[i] == 0 and len(arr) == 1:
+    if arr[-i] == 0 and len(arr) == 1:
         break
-    
-    answer += 2**(arr[-i]*i)
-    #print(i,arr[-i]*i,answer)
+    if arr[-i-1] == 0:
+        continue
+    answer += 2**(arr[-i-1]*i)
+    #print(i,arr[-i-1]*i,answer)
 
 print(answer)
