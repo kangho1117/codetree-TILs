f730ad7a1a319d7dfa50ebd90613893b19f89712
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-10-14 09:08:03 +0900

    Title: G or H 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241014/G or H 2/G-or-H-2.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main() {
+    int n,size=0;
+    cin>>n;
+    vector<pair<char,int>> arr;
+    for(int i=0,a;i<n;i++)
+    {
+        char c;
+        cin>>a>>c;
+        arr.push_back({a,c});
+    }
+    sort(arr.begin(), arr.end());
+    for(int k=1;k<=n;k++)
+{
+    for(int i=0;i<n;i++)
+    {
+        int H=0, G=0;
+        vector<pair<int,char>> temp;
+        for(int j=i;j<i+k && j<n;j++)
+        {
+            if(arr[j].second == 'H') {H++;temp.push_back({arr[j].first,'H'});}
+            else if(arr[j].second == 'G') {G++;temp.push_back({arr[j].first,'G'});}
+        }
+        if(H==G)
+        {
+            // for(auto &a : temp)
+            //     cout<<a.first<<" "<<a.second<<" ! ";
+            // cout<<"\n";
+            size = max(size, temp[temp.size()-1].first - temp[0].first);
+        }
+    }
+}
+    cout<<size;
+    return 0;
+}
