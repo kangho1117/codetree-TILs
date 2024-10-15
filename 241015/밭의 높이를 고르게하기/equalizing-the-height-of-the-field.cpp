23bf11db4008292fdd036d54693a46f71c1dc263
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-10-15 10:13:04 +0900

    Title: 밭의 높이를 고르게하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241015/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.cpp
+++ 241015/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.cpp
@@ -1,55 +1,30 @@
 #include <iostream>
-#include <list>
+#include <vector>
+#include <stdlib.h>
 #include <algorithm>
+#include <climits>
 using namespace std;
 int main() {
-    int n,h,t,oricnt=0,cost=0;
+    int n,h,t,cost=INT_MAX;
     cin>>n>>h>>t;
-    list<int> big,small;
+    vector<int> arr(n);
     for(int i=0,temp;i<n;i++)
+        cin>>arr[i];
+    for(int i=0;i<n-t;i++)
     {
-        cin>>temp;
-        if(temp>h) big.push_back(temp);
-        else if(temp<h) small.push_back(temp);
-        else oricnt++;
-    }
-    if(oricnt >= t) {cout<<cost; return 0;}
-    big.sort();
-    small.sort(greater<>());
-    while(t-oricnt>0)
-    {
-        if(big.empty() && small.empty()) break;
-        else if(big.empty())
+        int tempcost=0;
+        for(int j=i;j<i+t && j<n;j++)
         {
-            cost += h-small.front();
-            small.pop_front();
-            oricnt++;
-        }
-        else if(small.empty())
-        {
-            cost += h-big.front();
-            big.pop_front();
-            oricnt++;
-        }
-        else
-        {
-            if(h - small.front() > big.front() - h)
+            if(arr[j] == h)
             {
-                //cout<<"a";
-                cost += big.front()-h;
-                big.pop_front();
-                oricnt++;
-                //cout<<cost<<"\n";
+                continue;
             }
             else
             {
-                //cout<<"b";
-                cost += h-small.front();
-                small.pop_front();
-                oricnt++;
-                //cout<<cost<<"\n";
+                tempcost += abs(arr[j]-h);
             }
         }
+        cost = min(cost, tempcost);
     }
     cout<<cost;
     return 0;
