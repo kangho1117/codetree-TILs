c8c726f87e8f35e3e3bac14f8e2700c499ab80a4
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-10-15 14:24:56 +0900

    Title: 이상한 진수 2, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

--- 241015/이상한 진수 2/awkward-digits-2.py
+++ 241015/이상한 진수 2/awkward-digits-2.py
@@ -1,27 +1,49 @@
-arr = list(input())
+# arr = list(input())
 
-n = len(arr)
-for i in range(n):
-    arr[i] = int(arr[i])
+# n = len(arr)
+# for i in range(n):
+#     arr[i] = int(arr[i])
 
-for i in range(n):
-    #print(i, arr[i])
+# for i in range(n):
 
-    if arr[i] == 0:
-        arr[i] = 1
-        #print(i,arr)
-        break
-    if i == n - 1:
-        arr[i] = 0
+#     if arr[i] == 0:
+#         arr[i] = 1
+#         break
+#     if i == n - 1:
+#         arr[i] = 0
 
-answer = 0
-#print(arr[-7])
-for i in range(n-1,-1,-1):
-    if arr[-i] == 0 and len(arr) == 1:
-        break
-    if arr[-i-1] == 0:
-        continue
-    answer += 2**(arr[-i-1]*i)
-    #print(i,arr[-i-1]*i,answer)
+# answer = 0
+# for i in range(n-1,-1,-1):
+#     if arr[-i] == 0 and len(arr) == 1:
+#         break
+#     if arr[-i-1] == 0:
+#         continue
+#     answer += 2**(arr[-i-1]*i)
+# print(answer) 
 
-print(answer)
+import sys
+
+INT_MIN = -sys.maxsize
+
+# 변수 선언 및 입력
+binary = list(map(int, list(input())))
+length = len(binary)
+
+# 각 i번째 자릿수를 바꾸었을 때의 십진수 값을 구해줍니다.
+ans = INT_MIN
+for i in range(length):
+	# i번째 자릿수를 바꿉니다.
+	binary[i] = 1 - binary[i]
+	
+	# 십진수로 변환합니다.
+	num = 0
+	for j in range(length):
+		num = num * 2 + binary[j]
+	
+	ans = max(ans, num)
+	
+	# i번째 자릿수를 원래대로 돌려놓습니다.
+	binary[i] = 1 - binary[i]
+
+# 출력
+print(ans)
