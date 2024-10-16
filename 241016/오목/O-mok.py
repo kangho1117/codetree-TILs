9db93b5ab980dcd9201201d096c82683d46bbe47
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-10-16 16:10:54 +0900

    Title: 오목, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 241016/오목/O-mok.py
+++ 241016/오목/O-mok.py
@@ -4,17 +4,17 @@ answer1 = []
 for i in range(14):
     for j in range(14):
         #print(arr_2d[i][j])
-        if arr_2d[i][j] != 0 and  arr_2d[i+1][j] != 0 and  arr_2d[i+2][j] != 0 and  arr_2d[i+3][j] != 0 and arr_2d[i+4][j] != 0:
+        if arr_2d[i][j] != 0 and  arr_2d[i][j] == arr_2d[i+1][j] and arr_2d[i][j] == arr_2d[i+2][j] and arr_2d[i][j] == arr_2d[i+3][j] and arr_2d[i][j] == arr_2d[i+4][j]:
             answer1 = arr_2d[i][j]
             ans_i, ans_j = i + 1 + 2, j + 1
-        if arr_2d[i][j] != 0 and  arr_2d[i][j+1] != 0 and  arr_2d[i][j+2] != 0 and  arr_2d[i][j+3] != 0 and arr_2d[i][j+4] != 0:
+        if arr_2d[i][j] != 0 and arr_2d[i][j] == arr_2d[i][j+1] and arr_2d[i][j] == arr_2d[i][j+2] and arr_2d[i][j] == arr_2d[i][j+3] and arr_2d[i][j] == arr_2d[i][j+4]:
             answer1 = arr_2d[i][j]
             ans_i, ans_j = i + 1, j + 1 + 2
-        if arr_2d[i][j] != 0 and  arr_2d[i+1][j+1] != 0 and  arr_2d[i+2][j+2] != 0 and  arr_2d[i+3][j+3] != 0 and arr_2d[i+4][j+4] != 0:
+        if arr_2d[i][j] != 0 and arr_2d[i][j] == arr_2d[i+1][j+1] and arr_2d[i][j] == arr_2d[i+2][j+2] and arr_2d[i][j] == arr_2d[i+3][j+3] and arr_2d[i][j] == arr_2d[i+4][j+4]:
             answer1 = arr_2d[i][j]
             ans_i, ans_j = i + 1 + 2, j + 1 + 2
         if  j >= 4:
-            if arr_2d[i][j] != 0 and  arr_2d[i+1][j-1] != 0 and  arr_2d[i+2][j-2] != 0 and  arr_2d[i+3][j-3] != 0 and arr_2d[i+4][j-4] != 0:
+            if arr_2d[i][j] != 0 and arr_2d[i][j] == arr_2d[i+1][j-1] and arr_2d[i][j] == arr_2d[i+2][j-2] and arr_2d[i][j] == arr_2d[i+3][j-3] and arr_2d[i][j] == arr_2d[i+4][j-4]:
                 answer1 = arr_2d[i][j]
                 ans_i, ans_j = i + 1 + 2, j + 1 - 2
 
