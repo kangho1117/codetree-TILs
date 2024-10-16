1aac90a0d724ea3f7a2d4cf8701b105987d58bdf
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-10-16 11:23:11 +0900

    Title: 씨 오 더블유 2, Time: 120ms, Memory: 25MB, Status: Passed - CodeTree

--- 241016/씨 오 더블유 2/c-o-w-2.py
+++ 241016/씨 오 더블유 2/c-o-w-2.py
@@ -2,16 +2,17 @@ n = int(input())
 
 arr = list(input())
 cnt = 0
-for i in range(n):
-    #print(i)
-    if arr[i] == 'C':
-        for j in range(i+1,n):
-            #print(i, j)
-            if arr[j] == 'O':
-                for k in range(j+1,n):
-                    #print(i,j,k)
-                    if arr[k] =='W':
-                        cnt += 1
-                        #print(i,j,k,cnt)
+# for i in range(n):
+#     if arr[i] == 'C':
+#         for j in range(i+1,n):
+#             if arr[j] == 'O':
+#                 for k in range(j+1,n):
+#                     if arr[k] =='W':
+#                         cnt += 1
 
+for i in range(n):
+    for j in range(i+1,n):
+        for k in range(j+1,n):
+            if arr[i] =='C' and arr[j] =='O' and arr[k] =='W':
+                cnt += 1
 print(cnt)
