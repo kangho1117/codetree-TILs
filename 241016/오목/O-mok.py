c929800dea60219a3c1066e9b293f66188877563
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-10-16 16:56:10 +0900

    Title: 오목, Time: 130ms, Memory: 73MB, Status: Passed - CodeTree

--- 241016/오목/O-mok.py
+++ 241016/오목/O-mok.py
@@ -1,9 +1,6 @@
 import sys
 arr_2d = [list(map(int, input().split())) for _ in range(19)]
 
-answer1 = 0
-ans_i, ans_j = 0, 0
-
 def in_range(x, y):
     return 0 <= x and x < 19 and 0 <= y and y < 19
 
@@ -35,7 +32,3 @@ for i in range(19):
                 print(i + 2 * dx + 1 , j + 2 * dy + 1)
                 sys.exit()
 print(0)
-
-print(answer1)
-if ans_i != 0 and ans_j != 0:
-    print(ans_i, ans_j)
