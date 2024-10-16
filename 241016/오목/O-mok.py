cb47f9745aa951bff9a790c734becde37fb04104
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-10-16 16:03:14 +0900

    Title: 오목, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241016/오목/O-mok.py
@@ -0,0 +1,22 @@
+arr_2d = [list(map(int, input().split())) for _ in range(19)]
+
+answer1 = []
+for i in range(14):
+    for j in range(14):
+        #print(arr_2d[i][j])
+        if arr_2d[i][j] != 0 and  arr_2d[i+1][j] != 0 and  arr_2d[i+2][j] != 0 and  arr_2d[i+3][j] != 0 and arr_2d[i+4][j] != 0:
+            answer1 = arr_2d[i][j]
+            ans_i, ans_j = i + 1 + 2, j + 1
+        if arr_2d[i][j] != 0 and  arr_2d[i][j+1] != 0 and  arr_2d[i][j+2] != 0 and  arr_2d[i][j+3] != 0 and arr_2d[i][j+4] != 0:
+            answer1 = arr_2d[i][j]
+            ans_i, ans_j = i + 1, j + 1 + 2
+        if arr_2d[i][j] != 0 and  arr_2d[i+1][j+1] != 0 and  arr_2d[i+2][j+2] != 0 and  arr_2d[i+3][j+3] != 0 and arr_2d[i+4][j+4] != 0:
+            answer1 = arr_2d[i][j]
+            ans_i, ans_j = i + 1 + 2, j + 1 + 2
+        if  j >= 4:
+            if arr_2d[i][j] != 0 and  arr_2d[i+1][j-1] != 0 and  arr_2d[i+2][j-2] != 0 and  arr_2d[i+3][j-3] != 0 and arr_2d[i+4][j-4] != 0:
+                answer1 = arr_2d[i][j]
+                ans_i, ans_j = i + 1 + 2, j + 1 - 2
+
+print(answer1)
+print(ans_i, ans_j)
