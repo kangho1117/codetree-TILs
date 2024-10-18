aa248165ff4ccfd80c48b7d4983c75802ac8be95
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-10-18 11:23:34 +0900

    Title: 구간 중 최대 합, Time: 101ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 241018/구간 중 최대 합/max-sum-of-subarray.py
@@ -0,0 +1,14 @@
+n, k = map(int, input().split())
+
+arr = list(map(int, input().split()))
+
+max_val = 0
+
+for i in range(n-k+1):
+    cnt = 0
+    
+    for j in range(i,k+i):
+        cnt += arr[j]
+    
+    max_val = max(max_val, cnt)
+print(max_val)
