d2c8e8a76b7dde9fda91a5ca4ff1c23260cdba41
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-10-22 08:59:06 +0900

    Title: 개발자의 능력 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241022/개발자의 능력 2/ability-of-developer-2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main() {
+    vector<int> arr(6), vec(3);
+    for(int i=0;i<6;i++)
+        cin>>arr[i];
+    sort(arr.begin(), arr.end());
+    for(int i=0,j=5;i<=2,j>=3;i++,j--)
+    {
+        vec[i] = arr[i]+arr[j];
+    }
+    sort(vec.begin(), vec.end());
+    cout<<vec.back()-vec.front();
+    return 0;
+}
