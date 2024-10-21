80bab6b7ce336e397b91db3febe9bd7e547e63a8
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-10-21 10:48:10 +0900

    Title: 숫자 카운트, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241021/숫자 카운트/numeric-count.cpp
+++ 241021/숫자 카운트/numeric-count.cpp
@@ -4,10 +4,10 @@ int main()
 {
     int n,ans=0;
     cin>>n;
+    int a[n],b[n],c[n];
     for(int v=0;v<n;v++)
-    {
-        int a,b,c;
-        cin>>a>>b>>c;
+        cin>>a[v]>>b[v]>>c[v];
+        
         for(int i=1;i<=9;i++)
         {
             for(int j=1;j<=9;j++)
@@ -15,19 +15,28 @@ int main()
                 for(int k=1;k<=9;k++)
                 {
                     if(i == j || i == k || j == k) continue;
-                    int cnt1=0,cnt2=0,x=a/100,y=a%100/10,z=a%10;
-                    if(x==i) cnt1++;
-                    if(y==j) cnt1++;
-                    if(z==k) cnt1++;
-                    if(x==j || x==k) cnt2++;
-                    if(y==i || y==k) cnt2++;
-                    if(z==i || z==j) cnt2++;
+                    bool succeeded = true;
+                    for(int v=0;v<n;v++)
+                    {
+                        int cnt1=0,cnt2=0,x=a[v]/100,y=a[v]%100/10,z=a[v]%10;
+                        if(x==i) cnt1++;
+                        if(y==j) cnt1++;
+                        if(z==k) cnt1++;
+                        if(x==j || x==k) cnt2++;
+                        if(y==i || y==k) cnt2++;
+                        if(z==i || z==j) cnt2++;
 
-                    if(cnt1 == b && cnt2 == c) ans++;
+                        if(cnt1 != b[v] || cnt2 != c[v]) 
+                        {
+                            succeeded = false;
+                            break;
+                        }
+                    }
+                    if(succeeded) ans++;
                 }
             }
         }
-    }
+    
     cout<<ans;
     return 0;
 }
