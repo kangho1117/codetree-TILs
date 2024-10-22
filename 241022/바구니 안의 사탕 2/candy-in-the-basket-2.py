db321e5e63a1e7bac0f51f4df0a5f17b9f6d8d2e
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-10-22 17:50:06 +0900

    Title: 바구니 안의 사탕 2, Time: 105ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 241022/바구니 안의 사탕 2/candy-in-the-basket-2.py
+++ 241022/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -1,14 +1,19 @@
 n,k = map(int,input().split())
 
-bucket = [0] * 101
+bucket = [0] * 102
 
 for i in range(n):
     candy,cordinate = map(int,input().split())
     bucket[cordinate] = candy
 
 max_cnt = 0
-for c in range(k,len(bucket)-k):
-    cndy = sum(bucket[c-k:c+k+1])
+
+for c in range(k+1,len(bucket)-k):
+    #cndy = sum(bucket[c-k:c+k+1])
+    cndy = 0
+    for j in range(c-k, c+k+1):
+        cndy += bucket[j]
+    #print(c,cndy, max_cnt)    
     max_cnt = max(max_cnt, cndy)
 
 print(max_cnt)
