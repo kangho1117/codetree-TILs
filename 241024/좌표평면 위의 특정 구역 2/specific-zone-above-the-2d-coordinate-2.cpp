7e93c70420438ce59eff69b472bef3b30032c655
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-10-24 09:33:39 +0900

    Title: 좌표평면 위의 특정 구역 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241024/좌표평면 위의 특정 구역 2/specific-zone-above-the-2d-coordinate-2.cpp
+++ 241024/좌표평면 위의 특정 구역 2/specific-zone-above-the-2d-coordinate-2.cpp
@@ -5,9 +5,9 @@
 using namespace std;
 bool cmp(pair<int,int>&a, pair<int,int>&b)
 {
-    if(a.first==b.first)
-        return a.second<b.second;
-    return a.first<b.first;
+    if(a.second==b.second)
+        return a.first<b.first;
+    return a.second<b.second;
 }
 int main() {
     int n,area=INT_MAX;
@@ -21,6 +21,28 @@ int main() {
     }       
     sort(arr.begin(), arr.end());
     int tmin1=INT_MAX,tmax1=0,tmin2=INT_MAX,tmax2=0;
+    for(int i=0;i<n-1;i++)
+    {
+        auto a = arr[i];
+        tmin1 = min(tmin1,a.first);
+        tmax1 = max(tmax1,a.first);
+        tmin2 = min(tmin2,a.second);
+        tmax2 = max(tmax2,a.second);
+    }
+    area = min(area,(tmax1-tmin1)*(tmax2-tmin2));
+    tmin1=INT_MAX,tmax1=0,tmin2=INT_MAX,tmax2=0;
+    for(int i=1;i<n;i++)
+    {
+        auto a = arr[i];
+        tmin1 = min(tmin1,a.first);
+        tmax1 = max(tmax1,a.first);
+        tmin2 = min(tmin2,a.second);
+        tmax2 = max(tmax2,a.second);
+    }
+    area = min(area,(tmax1-tmin1)*(tmax2-tmin2));
+
+    sort(arr.begin(), arr.end(), cmp);
+    tmin1=INT_MAX,tmax1=0,tmin2=INT_MAX,tmax2=0;
     for(int i=0;i<n-1;i++)
     {
         auto a = arr[i];
