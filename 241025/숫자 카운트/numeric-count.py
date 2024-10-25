df34c01d3976c73d5163a342d1965a45353f01c5
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-10-25 14:10:42 +0900

    Title: 숫자 카운트, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241025/숫자 카운트/numeric-count.py
+++ 241025/숫자 카운트/numeric-count.py
@@ -9,21 +9,36 @@ for i in range(1,10):
         for k in range(1,10):
             if i == j or j == k or k == i:
                 continue
-            one_cnt = 0
-            two_cnt = 0
             success = True
             for num, cnt1,cnt2 in arr_2d:
+
+                one_cnt = 0
+                two_cnt = 0
                 a = num // 100
                 b = num // 10 % 10
                 c = num % 10
+                #print(a,b,c)
             
-                if i == a or j == b or k == c:
+                # if a == i or b == j or c == k:
+                #     one_cnt += 1
+                if a == i:
                     one_cnt += 1
-                elif i in num or j in num or k in num:
+                if b == j:
+                    one_cnt += 1                    
+                if c == k:
+                    one_cnt += 1
+                if (i != a and i in (a,b,c)) or (j != b and  j in (a,b,c)) or (k != c and k in (a,b,c)):
                     two_cnt += 1
+                # if a == j or a == k:
+                #     two_cnt += 1
+                # if b == i or b == k:
+                #     two_cnt += 1
+                # if c == i or c == j:
+                #     two_cnt += 1
             
-                if one_cnt != cnt1 and two_cnt != cnt2:
+                if one_cnt != cnt1 or two_cnt != cnt2:
                     success =False
+                    break
             if success :
                 cnt += 1
 
