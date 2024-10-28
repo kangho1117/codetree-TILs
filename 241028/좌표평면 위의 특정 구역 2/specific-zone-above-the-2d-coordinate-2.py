823f79582894706fe013c556b322c73b35cfdd7b
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-10-28 17:52:51 +0900

    Title: 좌표평면 위의 특정 구역 2, Time: 121ms, Memory: 25MB, Status: Passed - CodeTree

--- 241028/좌표평면 위의 특정 구역 2/specific-zone-above-the-2d-coordinate-2.py
+++ 241028/좌표평면 위의 특정 구역 2/specific-zone-above-the-2d-coordinate-2.py
@@ -1,8 +1,10 @@
+import sys
+INT_MAX = sys.maxsize
 n = int(input())
 
 arr_2d = [tuple(map(int, input().split())) for _ in range(n)]
 
-min_area = 0
+min_area = INT_MAX
 for i in range(n):
     area = 0
 
@@ -15,7 +17,9 @@ for i in range(n):
         x, y= arr_2d[j]
         x_list.append(x)
         y_list.append(y)
+    
     area = (max(x_list) - min(x_list)) * (max(y_list) - min(y_list))
     min_area = min(min_area, area)
+    #print(x_list,y_list,area,min_area)
 
-print(area)
+print(min_area)
