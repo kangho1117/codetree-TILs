41a986584dd6c1837b3f99131512491c56d1c0b9
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-10-31 10:33:01 +0900

    Title: 겹치지 않는 선분 2, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241031/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.py
+++ 241031/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.py
@@ -10,9 +10,18 @@ arr_2d.sort()
 answer = n
 
 for i in range(n):
+    ol_cnt = 0
     for j in range(i+1,n):
-        if arr_2d[i][1][0] >= arr_2d[j][1][0]:
-            answer -= 2
+        if answer == 0:
             break
+        if arr_2d[i][1][0] >= arr_2d[j][1][0]:
+            
+            if ol_cnt == 0:
+                answer -= 2
+                ol_cnt += 1
+            else :
+                answer -= 1
+            #print(i,j,arr_2d[i],arr_2d[j],ol_cnt,answer)
+            continue
 
 print(answer)
