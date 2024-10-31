c4eea02a0828bebb3d9d37b9368bbdc413fdfc9e
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-10-31 09:41:44 +0900

    Title: 겹치지 않는 선분 2, Time: 24ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 241031/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.cpp
+++ 241031/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 int main() {
-    int n,arr[3000001][2]={0,};
+    int n,arr[2000001][2]={0,};
     cin>>n;
     int ans = n, check[n+1]={0,};
     for(int i=1,x1,x2;i<=n;i++)
@@ -11,19 +11,20 @@ int main() {
         arr[x2+1000000][1] = i;
     }
     check[0] = 1;
-    for(int i=0,mem=0;i<=3000000;i++)
+    for(int i=0,mem=0;i<=2000000;i++)
     {
+        if(ans == 0) break;
         int x1 = arr[i][0];
         if(check[x1] == 0 && x1>0)
         {
             bool cross = false;
             check[x1] = 1;
-            for(int j=mem;j<=3000000;j++)
+            for(int j=mem;j<=2000000;j++)
             {
                 int x2 = arr[j][1];
                 if(x2 == x1) 
                 {
-                    if(!cross) mem = j;
+                    if(!cross) mem = j+1;
                     break;
                 }
                 if(x2 != 0 && x2 != x1 && check[x2] == 0)
