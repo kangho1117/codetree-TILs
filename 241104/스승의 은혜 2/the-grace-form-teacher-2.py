db8d1903fe4d16987b2930a20fa0b1f50750658f
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-11-04 09:56:00 +0900

    Title: 스승의 은혜 2, Time: 60ms, Memory: 16MB, Status: Passed - CodeTree

--- 241104/스승의 은혜 2/the-grace-form-teacher-2.py
+++ 241104/스승의 은혜 2/the-grace-form-teacher-2.py
@@ -2,15 +2,18 @@ n, b = map(int, input().split())
 
 arr = [int(input()) for _ in range(n)]
 arr.sort()
+#print(arr)
 answer = 1
 for i in range(n):
     money_sum = arr[i] / 2
+    student = 1
     for j in range(n):
         if i == j :
             continue
-        if  money_sum + arr[j] <= 18:
+        if  money_sum + arr[j] <= b:
+            student += 1
             money_sum += arr[j]
-            answer = max(answer, j+1)
+            answer = max(answer, student)
             #print(money_sum,answer)
         else :
             break
