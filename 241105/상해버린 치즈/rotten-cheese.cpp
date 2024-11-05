a61f913f54a0c287af0474b43e1714404b710269
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-11-05 10:40:40 +0900

    Title: 상해버린 치즈, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241105/상해버린 치즈/rotten-cheese.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+using namespace std;
+int n,m,d,s,cheese[51][51]={0,},sick[51]={0,},maxnum=0;
+void print()
+{
+    for(int i=1;i<=5;i++)
+    {
+        for(int j=1;j<=10;j++)
+        {
+            cout<<cheese[i][j]<<" ";
+        }
+        cout<<"\n";
+    }
+    cout<<"\n";
+    for(int i=1;i<=5;i++)
+    {
+        cout<<sick[i]<<" ";
+    }
+    cout<<"\n";
+}
+int main() {
+    cin>>n>>m>>d>>s;
+    for(int i=0;i<d;i++)
+    {
+        int p,m,t;
+        cin>>p>>m>>t;
+        if(cheese[m][p] == 0 || t < cheese[m][p]) cheese[m][p] = t;
+    }
+    for(int i=0;i<s;i++)
+    {   
+        int p,t;
+        cin>>p>>t;
+        sick[p] = t;
+    }
+    //print();
+    for(int i=1;i<=50;i++)
+    {
+        bool find = true;
+        for(int j=1;j<=50;j++)
+        {
+            if(cheese[i][j] != 0)
+            {
+                int eattime = cheese[i][j];
+                if(sick[j] != 0 && eattime >= sick[j])
+                {
+                    find = false;
+                    break;
+                }
+            }
+        }
+        if(find)
+        {
+            int temp=0;
+            for(int j=1;j<=50;j++)
+                if(cheese[i][j] != 0)
+                    temp++;
+            if(temp > maxnum) maxnum = temp;
+        }
+    }
+    cout<<maxnum;
+    return 0;
+}
