08b253f33aafb36431928acc7024084b2c61887a
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-11-07 09:59:11 +0900

    Title: 이상한 폭탄 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241107/이상한 폭탄 2/strange-bomb-2.cpp
+++ 241107/이상한 폭탄 2/strange-bomb-2.cpp
@@ -9,13 +9,9 @@ int main() {
     for(int i=0;i<n;i++)
     {
         for(int j=i+1;j<=i+k;j++)
-        {
             if(arr[i] == arr[j] && arr[i]> bomb) bomb = arr[i];
-        }
         for(int j=i-1;j>=0 && j>=i-k;j--)
-        {
             if(arr[i] == arr[j] && arr[i]> bomb) bomb = arr[i];
-        }
     }
     cout<<bomb;
     return 0;
