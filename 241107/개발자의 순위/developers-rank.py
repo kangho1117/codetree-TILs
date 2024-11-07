0bc871a9abe692e196e76ad34f00bd6ea65e3510
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-11-07 16:32:12 +0900

    Title: 개발자의 순위, Time: 45ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 241107/개발자의 순위/developers-rank.py
@@ -0,0 +1,21 @@
+k, n = map(int, input().split())
+
+arr_2d = [list(map(int, input().split())) for _ in range(k)]
+
+ans = 0
+for i in range(1, n+1):
+    for j in range(1,n+1):
+        if i == j:
+            correct = True
+        
+        for lists in arr_2d:
+            index_i = lists.index(i)
+            index_j = lists.index(j)
+            if index_i > index_j:
+                correct = False
+        if correct:
+            ans += 1
+print(ans)
+
+
+#print(ranking_arr)
