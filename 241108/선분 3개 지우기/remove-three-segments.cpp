59f82dc7ec36b21fcd3057436c95c9fbeb2f8ff8
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-11-08 10:53:27 +0900

    Title: 선분 3개 지우기, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

--- 241108/선분 3개 지우기/remove-three-segments.cpp
+++ 241108/선분 3개 지우기/remove-three-segments.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 int main() {
     int n,maxnum=0;
@@ -11,32 +12,23 @@ int main() {
         cin>>x>>y;
         vec[i] = {x,y};
     }
-
+    sort(vec.begin(), vec.end());
     for(int i=0;i<n;i++)
     {
-        for(int j=0;j<n;j++)
+        for(int j=i+1;j<n;j++)
         {
-            if(i==j) continue;
-            for(int k=0;k<n;k++)
+            for(int k=j+1;k<n;k++)
             {
-                if(i==k || j==k)continue;
                 bool find = true;
-                for(int w=0,p=1;w<n-1;w++)
+                int arr[101]={0,};
+                for(int w=0;w<n;w++)
                 {
                     if(w==i || w==j || w==k) continue;
-                    if(w+p==i || w+p==j || w+p==k) 
-                    {
-                        w--;
-                        p++;
-                    }
-                    else p=1;
-                    if(w+p>=n) continue;
-                    if(vec[w].second >= vec[w+p].first)
-                    {
-                        find = false;
-                        break;
-                    }
+                    for(int z=vec[w].first;z<=vec[w].second;z++)
+                        arr[z]++;
                 }
+                for(int z=0;z<=100;z++)
+                    if(arr[z]>1) find = false;
                 if(find) maxnum++;
             }
         }
