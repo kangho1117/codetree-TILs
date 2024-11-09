f0926e14e5f9e320298ab57b3b76bada76d78264
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-11-09 11:46:48 +0900

    Title: 스승의 은혜 3, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241109/스승의 은혜 3/the-grace-form-teacher-3.cpp
+++ 241109/스승의 은혜 3/the-grace-form-teacher-3.cpp
@@ -7,6 +7,11 @@ bool cmp(pair<int,int> a, pair<int,int> b)
     if(a.first == b.first) return a.second < b.second;
     return a.first < b.first;
 }
+bool cmp2(pair<int,int> a, pair<int,int> b)
+{
+    if(a.second == b.second) return a.first < b.first;
+    return a.second < b.second;
+}
 int main() {
     int n,b,maxnum=0;
     cin>>n>>b;
@@ -23,7 +28,7 @@ int main() {
         v[i].first /=2;
         for(int j=1, money = b;j<=n;j++)
         {
-            money -= v[j].first + v[j].second;
+            money -= (v[j].first + v[j].second);
             if(money<0)
             {
                 if(maxnum < j-1) maxnum = j-1;
@@ -31,6 +36,21 @@ int main() {
             }
         }
         v[i].first *= 2;
+    }
+    sort(v.begin(), v.end(),cmp2);
+    for(int i=1;i<=n;i++)
+    {
+        v[i].second /=2;
+        for(int j=1, money = b;j<=n;j++)
+        {
+            money -= (v[j].first + v[j].second);
+            if(money<0)
+            {
+                if(maxnum < j-1) maxnum = j-1;
+                break;
+            }
+        }
+        v[i].second *= 2;
     }
     cout<<maxnum;
     return 0;
