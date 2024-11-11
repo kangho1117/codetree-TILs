559e0fb212ed8e246ce8d0c86e741799101dccdb
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-11-11 10:13:58 +0900

    Title: 스승의 은혜 3, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241111/스승의 은혜 3/the-grace-form-teacher-3.cpp
+++ 241111/스승의 은혜 3/the-grace-form-teacher-3.cpp
@@ -2,62 +2,46 @@
 #include <algorithm>
 #include <vector>
 using namespace std;
-bool cmp(vector<int> &a, vector<int> &b)
-{
-    if(a[0] == b[0]) return a[1] < b[1];
-    return a[0] < b[0];
-}
-int repeat(vector<vector<int>> arr, int budget, int i, int n)
+int repeat(vector<vector<int>> vec,int budget, int n)
 {
     int cnt=0;
-    arr[i][0]/=2;
-    sort(arr.begin(), arr.end(),cmp);
+    sort(vec.begin(), vec.end());
     for(int j=0;j<n;j++)
+    {
+        if(budget - vec[j][0] >= 0)
         {
-            int price = arr[j][0]+arr[j][1];
-            if(budget - price >= 0)
-            {
-                budget -= price;
-                cnt++;
-            }
+            budget -= vec[j][0];
+            cnt++;
         }
+    }
     return cnt;
 }
 int main() {
     int n,b,ans=0;
     cin>>n>>b;
-    vector<vector<int>> arr(n,vector<int>(2,0)), arr2(n,vector<int>(2,0));
+    vector<vector<int>> arr(n,vector<int>(3,0)), arr2(n,vector<int>(3,0));
     for(int i=0,a,b;i<n;i++)
     {
         cin>>a>>b;
-        arr[i][0] = a;
-        arr[i][1] = b;
-        arr2[i][0] = b;
-        arr2[i][1] = a;
+        arr[i][1] = a;
+        arr[i][2] = b;
+        arr[i][0] = a+b;
+        arr2[i][1] = b;
+        arr2[i][2] = a;
+        arr2[i][0] = a+b;
     }
-    // sort(arr.begin(), arr.end(), cmp);
-    // for(int i=0;i<n;i++)
-    //     cout<<arr[i][0]<<" "<<arr[i][1]<<"\n";
     for(int i=0;i<n;i++)
     {
-        int cnt = repeat(arr,b,i,n);
+        arr[i][0] = arr[i][1]/2 + arr[i][2];
+        int cnt = repeat(arr,b,n);
+        arr[i][0] = arr[i][1] + arr[i][2];
         if(cnt > ans) ans = cnt;
     }
-    sort(arr2.begin(), arr2.end(),cmp);
     for(int i=0;i<n;i++)
     {
-        arr2[i][1]/=2;
-        int cnt=0,budget=b;
-        for(int j=0;j<n;j++)
-        {
-            int price = arr2[j][0]+arr2[j][1];
-            if(budget - price >= 0)
-            {
-                budget -= price;
-                cnt++;
-            }
-        }
-        arr2[i][1]*=2;
+        arr2[i][0] = arr2[i][1] + arr2[i][2]/2;
+        int cnt = repeat(arr2,b,n);
+        arr2[i][0] = arr2[i][1] + arr2[i][2];
         if(cnt > ans) ans = cnt;
     }
     cout<<ans;
