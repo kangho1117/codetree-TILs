1ed9a7d6ebf3a4b5fda0a847127e1e342a057f8e
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-11-11 09:03:47 +0900

    Title: 스승의 은혜 3, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241111/스승의 은혜 3/the-grace-form-teacher-3.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main() {
+    int n,b,ans=0;
+    cin>>n>>b;
+    int arr[n][2];
+    for(int i=0;i<n;i++)
+        cin>>arr[i][0]>>arr[i][1];
+    
+    for(int i=0;i<n;i++)
+    {
+        arr[i][0]/=2;
+        int cnt=0,budget=b;
+        for(int j=0;j<n;j++)
+        {
+            int price = arr[j][0]+arr[j][1];
+            if(budget - price >= 0)
+            {
+                budget -= price;
+                cnt++;
+            }
+            else break;
+        }
+        arr[i][0]*=2;
+        if(cnt > ans) ans = cnt;
+    }
+    cout<<ans;
+    return 0;
+}
