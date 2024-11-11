85cf3b75e355aa4b773f95855a44d2501c708653
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-11-11 09:12:58 +0900

    Title: 스승의 은혜 3, Time: 9ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241111/스승의 은혜 3/the-grace-form-teacher-3.cpp
+++ 241111/스승의 은혜 3/the-grace-form-teacher-3.cpp
@@ -5,9 +5,15 @@ using namespace std;
 int main() {
     int n,b,ans=0;
     cin>>n>>b;
-    vector<vector<int>> arr(n,vector<int>(2,0));
-    for(int i=0;i<n;i++)
-        cin>>arr[i][0]>>arr[i][1];
+    vector<vector<int>> arr(n,vector<int>(2,0)),arr2(n,vector<int>(2,0));
+    for(int i=0,a,b;i<n;i++)
+    {
+        cin>>a>>b;
+        arr[i][0] = a;
+        arr[i][1] = b;
+        arr2[i][0] = b;
+        arr2[i][1] = a;
+    }
     sort(arr.begin(), arr.end());
     for(int i=0;i<n;i++)
     {
@@ -23,6 +29,23 @@ int main() {
             }
         }
         arr[i][0]*=2;
+        if(cnt > ans) ans = cnt;
+    }
+    sort(arr2.begin(), arr2.end());
+    for(int i=0;i<n;i++)
+    {
+        arr2[i][1]/=2;
+        int cnt=0,budget=b;
+        for(int j=0;j<n;j++)
+        {
+            int price = arr2[j][0]+arr2[j][1];
+            if(budget - price >= 0)
+            {
+                budget -= price;
+                cnt++;
+            }
+        }
+        arr2[i][1]*=2;
         if(cnt > ans) ans = cnt;
     }
     cout<<ans;
