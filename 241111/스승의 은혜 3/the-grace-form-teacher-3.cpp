969d9be0f9f5dff2a420256b3c8b4aa3efa626e3
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-11-11 09:53:09 +0900

    Title: 스승의 은혜 3, Time: 78ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241111/스승의 은혜 3/the-grace-form-teacher-3.cpp
+++ 241111/스승의 은혜 3/the-grace-form-teacher-3.cpp
@@ -2,24 +2,12 @@
 #include <algorithm>
 #include <vector>
 using namespace std;
-int main() {
-    int n,b,ans=0;
-    cin>>n>>b;
-    vector<vector<int>> arr(n,vector<int>(2,0)),arr2(n,vector<int>(2,0));
-    for(int i=0,a,b;i<n;i++)
-    {
-        cin>>a>>b;
-        arr[i][0] = a;
-        arr[i][1] = b;
-        arr2[i][0] = b;
-        arr2[i][1] = a;
-    }
+int repeat(vector<vector<int>> arr, int budget, int i, int n)
+{
+    int cnt=0;
+    arr[i][0]/=2;
     sort(arr.begin(), arr.end());
-    for(int i=0;i<n;i++)
-    {
-        arr[i][0]/=2;
-        int cnt=0,budget=b;
-        for(int j=0;j<n;j++)
+    for(int j=0;j<n;j++)
         {
             int price = arr[j][0]+arr[j][1];
             if(budget - price >= 0)
@@ -28,7 +16,24 @@ int main() {
                 cnt++;
             }
         }
-        arr[i][0]*=2;
+    return cnt;
+}
+int main() {
+    int n,b,ans=0;
+    cin>>n>>b;
+    vector<vector<int>> arr(n,vector<int>(2,0)), arr2(n,vector<int>(2,0));
+    for(int i=0,a,b;i<n;i++)
+    {
+        cin>>a>>b;
+        arr[i][0] = a;
+        arr[i][1] = b;
+        arr2[i][0] = b;
+        arr2[i][1] = a;
+    }
+
+    for(int i=0;i<n;i++)
+    {
+        int cnt = repeat(arr,b,i,n);
         if(cnt > ans) ans = cnt;
     }
     sort(arr2.begin(), arr2.end());
