7cf3d8ac0426ff2f4f07a4a8a37dfb70cc0be150
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-11-14 10:39:29 +0900

    Title: 흥미로운 숫자 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241114/흥미로운 숫자 2/interesting-numbers-2.cpp
+++ 241114/흥미로운 숫자 2/interesting-numbers-2.cpp
@@ -4,18 +4,18 @@ using namespace std;
 int main() {
     int X,Y,cnt=0;
     cin>>X>>Y;
-    for(int i = X;i<=Y;i++)
+    for(int i = X; i<=Y; i++)
     {
         int arr[10] = {0,};
         string str = to_string(i);
         for(char &a : str)
             arr[a-48]++;
         bool other=false, one=false;
-        for(int j=0;j<10;j++)
+        for(int j=0;j<=9;j++)
         {
             if(arr[j]==1) one = true;
             else if(other == false && arr[j] > 1) other = true;
-            else if(other == true && arr[j] > 0) 
+            else if((one == true || other == true) && arr[j] > 0) 
             {
                 one = false;
                 other = false;
