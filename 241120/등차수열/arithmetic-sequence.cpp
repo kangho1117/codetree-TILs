8a198090958abb007f10ba78c03c8ff1e91494e5
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-11-20 13:55:49 +0900

    Title: 등차수열, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241120/등차수열/arithmetic-sequence.cpp
+++ 241120/등차수열/arithmetic-sequence.cpp
@@ -7,22 +7,10 @@ int main() {
     int arr[n];
     for(int i=0;i<n;i++)
         cin>>arr[i];
-
-        for(int k=1;k<=200;k++)
-        {
-            int cnt = 0;
-            for(int i=0;i<n-1;i++)
-            {
-            for(int j=i+1;j<n;j++)
-            {
-                if(arr[j] - k == k - arr[i])
-                {
-                    cnt++;
-                    // cout<<cnt<<"\n";
-                    // cout<<arr[j]<<" "<<arr[i]<<" "<<j<<" "<<i<<" "<<k<<"\n";
-                }
-            }
-        }
+    for(int k=1;k<=200;k++)
+    {
+        int cnt = 0;
+        for(int i=0;i<n-1;i++) for(int j=i+1;j<n;j++) if(arr[j] - k == k - arr[i]) cnt++;
         ans = max(ans,cnt);
     }
     cout<<ans;
