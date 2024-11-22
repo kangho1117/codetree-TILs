eb574abf4033b77b382ef85023652fe1db955444
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-11-22 10:50:48 +0900

    Title: 팰린드롬 수 찾기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241122/팰린드롬 수 찾기/find-the-number-of-palindrome.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    int x,y,cnt=0;
+    cin>>x>>y;
+    for(int i=x;i<=y;i++)
+    {
+        string str = to_string(i);
+        bool find = true;
+        for(int a=0,b=str.size()-1;b>0,a<str.size();a++,b--)
+        {
+            if(str[a] != str[b]) find = false;
+        }
+        if(find) cnt++;
+    }
+    cout<<cnt;
+    return 0;
+}
