77c514c90b39fc741cf031015599fa4aa84dc681
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-11-26 10:53:09 +0900

    Title: 3개의 선 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241126/3개의 선 2/three-lines-2.cpp
+++ 241126/3개의 선 2/three-lines-2.cpp
@@ -2,16 +2,11 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
-bool cmp(pair<int,int> &a,pair<int,int> &b)
-{
-    if(a.second == b.second) return a.first < b.first;
-    return a.second < b.second;
-}
 int main() {
     int n;
     cin>>n;
     vector<pair<int,int>> vec;
-    vector<pair<char,int>>  line; 
+    vector<pair<char,int>> line; 
     for(int i=0;i<n;i++)
     {
         int x,y;
@@ -36,8 +31,10 @@ int main() {
                     if(cntx > cnty) line.push_back({'x',a.first});
                     else line.push_back({'y',a.second});
                 }
+                a.first = -1;
+                a.second = -1;
             }
-            else
+            else if(b.first == 'y')
             {
                 if(a.first == b.second) continue;
                 else
@@ -51,13 +48,23 @@ int main() {
                     if(cntx > cnty) line.push_back({'x',a.first});
                     else line.push_back({'y',a.second});
                 }
+                a.first = -1;
+                a.second = -1;
             }
+        }
+        if(line.empty())
+        {
+            int cntx=0, cnty=0;
+            for(auto &v : vec)
+            {
+                if(a.first == v.first) cntx++;
+                else if(a.second == v.second) cnty++;
+            }
+            if(cntx > cnty) line.push_back({'x',a.first});
+            else line.push_back({'y',a.second});
         }
     }
     if(line.size() <= 3) cout<<1;
     else cout<<0;
     return 0;
 }
-//기존에 존재하는 선에 해당 점이 걸리는지 판단
-//안 걸린다면 나랑 같은 선에 존재하는 점 중에 어느 선쪽이 더 많은지 판단
-//더 많은 쪽으로 선을 생성
