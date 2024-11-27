42eeb4e19ecbcfe2a0f80b9d0f8d846af382cb6f
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-11-27 13:16:19 +0900

    Title: 좌표평면 위의 균형 2, Time: 18ms, Memory: 0MB, Status: Passed - CodeTree

--- 241127/좌표평면 위의 균형 2/balance-on-coordinate-plane-2.cpp
+++ 241127/좌표평면 위의 균형 2/balance-on-coordinate-plane-2.cpp
@@ -1,7 +1,47 @@
 #include <iostream>
+#include <map>
+#include <cmath>
 using namespace std;
-
 int main() {
-    // 여기에 코드를 작성해주세요.
+    int n,ans=9999;
+    cin>>n;
+    pair<int,int> arr[n];
+    for(int i=0,x,y;i<n;i++)
+    {
+        cin>>x>>y;
+        arr[i] = {x,y};
+    }
+
+    for(int i=2;i<=100;i+=2)
+    {
+        for(int j=2;j<=100;j+=2)
+        {
+            map<pair<int,int>,bool> mp1,mp2,mp3,mp4;
+            for(int k=0;k<n;k++)
+            {
+                int x = arr[k].first;
+                int y = arr[k].second;
+                if(x>i && y > j)//1
+                {
+                    mp1.insert({{x,y},1});
+                }
+                else if(x<=i && y > j)//2
+                {
+                    mp2.insert({{x,y},1});
+                }
+                else if(x>i && y <= j)//3
+                {
+                    mp3.insert({{x,y},1});
+                }
+                else if(x <= i && y <= j)//4
+                {
+                    mp4.insert({{x,y},1});
+                }
+            }
+            int cnt = max(mp1.size(),max(mp2.size(),max(mp3.size(),mp4.size())));
+            if(cnt < ans) ans = cnt;
+        }
+    }
+    cout<<ans;
     return 0;
 }
