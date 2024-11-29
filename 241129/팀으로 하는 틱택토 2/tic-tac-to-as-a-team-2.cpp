3f1ea7d7424e20521e7673a45cbeecf7ccbbe621
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-11-29 16:18:27 +0900

    Title: 팀으로 하는 틱택토 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241129/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.cpp
+++ 241129/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.cpp
@@ -9,44 +9,27 @@ int main() {
 	for(int i=1; i<=3; i++)
 		for(int j=1; j<=3; j++)
 			cin>>arr[i][j];
-
 	int num=0,ans=0;
 	for(int i=1; i<=3; i++)
 	{
 		for(int j=1; j<=3; j++)
-		{
 			v[num].insert({arr[i][j],1});
-
-		}
 		num++;
 	}
-
 	for(int i=1; i<=3; i++)
 	{
 		for(int j=1; j<=3; j++)
-		{
 			v[num].insert({arr[j][i],1});
-	
-		}
 		num++;
 	}
-
-	for(int i=1,j=1; i<=3; i++,j++)
-	{
+	for(int i=1,j=1; i<=3&&j<=3; i++,j++)
 		v[num].insert({arr[i][j],1});
-	}
 	num++;
-	for(int i=1,j=3; i<=1; i++,j--)
-	{
+	for(int i=1,j=3; i<=3&&j>=1; i++,j--)
 		v[num].insert({arr[i][j],1});
-
-
-	}
 	num++;
 	for(auto &a : v)
-	{
 		if(a.size()==2) ans++;
-	}
 	cout<<ans;
 	return 0;
 }
