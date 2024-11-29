f4e4583d12f0467eb66f141c48cf67cd412d4df3
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-11-29 16:10:14 +0900

    Title: 팀으로 하는 틱택토 2, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241129/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <algorithm>
+using namespace std;
+int main() {
+	char arr[4][4]= {0,};
+	vector<map<int,int>> v(8,map<int,int>());
+	for(int i=1; i<=3; i++)
+		for(int j=1; j<=3; j++)
+			cin>>arr[i][j];
+
+	int num=0,ans=0;
+	for(int i=1; i<=3; i++)
+	{
+		for(int j=1; j<=3; j++)
+		{
+			v[num].insert({arr[i][j],1});
+
+		}
+		num++;
+	}
+
+	for(int i=1; i<=3; i++)
+	{
+		for(int j=1; j<=3; j++)
+		{
+			v[num].insert({arr[j][i],1});
+	
+		}
+		num++;
+	}
+
+	for(int i=1,j=1; i<=3; i++,j++)
+	{
+		v[num].insert({arr[i][j],1});
+	}
+	num++;
+	for(int i=1,j=3; i<=1; i++,j--)
+	{
+		v[num].insert({arr[i][j],1});
+
+
+	}
+	num++;
+	for(auto &a : v)
+	{
+		if(a.size()==2) ans++;
+	}
+	cout<<ans;
+	return 0;
+}
