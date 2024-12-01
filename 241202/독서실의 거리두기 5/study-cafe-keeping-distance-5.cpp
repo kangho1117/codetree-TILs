1e039a5804370d386275a2d411dd451c3a72f489
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-12-02 02:35:57 +0900

    Title: 독서실의 거리두기 5, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241202/독서실의 거리두기 5/study-cafe-keeping-distance-5.cpp
+++ 241202/독서실의 거리두기 5/study-cafe-keeping-distance-5.cpp
@@ -4,27 +4,35 @@ using namespace std;
 int main() {
     int n,ans=0;
     cin>>n;  
-    vector<char> v(n+2,'1'); 
-    for(int i=1;i<=n;i++)
+    vector<char> v(n); 
+    for(int i=0;i<n;i++)
         cin>>v[i];
     for(int i=0;i<n;i++)
     {
         if(v[i]=='0')
         {
-            int cnt1=0,cnt2=0,cnt3=0;
-            for(int a=i-1;a>=0;a--)
+            v[i] == '1';
+            for(int j=0;j<n;j++)
             {
-                cnt1++;
-                if(v[a]=='1') break;
+                if(v[j] == '1')
+                {
+                    int cnt1=0,cnt2=0,cnt3=0;
+                    for(int a = j-1;j>=0;j--)
+                    {
+                        cnt1++;
+                        if(v[j]=='1') break;
+                    }
+                    for(int a = j+1;j<n;j++)
+                    {
+                        cnt2++;
+                        if(v[j]=='1') break;
+                    }
+                    if(cnt1 < cnt2) cnt3 = cnt1;
+                    else cnt3 = cnt2;
+                    if(ans < cnt3) ans = cnt3;
+                }
             }
-            for(int a=i+1;a<=n+1;a++)
-            {
-                cnt2++;
-                if(v[a]=='1') break;
-            }
-            if(cnt1 > cnt2) cnt3 = cnt2;
-            else cnt3 = cnt1;
-            if(ans < cnt3) ans = cnt3; 
+            v[i] == '0';
         }
     }
     cout<<ans;
