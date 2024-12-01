061b2ed7035d602e1ad58b343b90505205815454
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-12-02 02:21:19 +0900

    Title: 독서실의 거리두기 5, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241202/독서실의 거리두기 5/study-cafe-keeping-distance-5.cpp
+++ 241202/독서실의 거리두기 5/study-cafe-keeping-distance-5.cpp
@@ -4,8 +4,8 @@ using namespace std;
 int main() {
     int n,ans=0;
     cin>>n;  
-    vector<char> v(n); 
-    for(int i=0;i<n;i++)
+    vector<char> v(n+2,'1'); 
+    for(int i=1;i<=n;i++)
         cin>>v[i];
     for(int i=0;i<n;i++)
     {
