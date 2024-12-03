5c30df0ee0e99b7bea45f3f8198ebfc31aa56331
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-12-03 09:59:20 +0900

    Title: 스승의 은혜 3, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

--- 241203/스승의 은혜 3/the-grace-form-teacher-3.py
+++ 241203/스승의 은혜 3/the-grace-form-teacher-3.py
@@ -2,6 +2,8 @@ n, m = map(int, input().split())
 
 student_arr = [list(map(int ,input().split())) for _ in range(n)]
 answer = 0
+student_arr.sort(key = lambda x : (x[0] + x[1]))
+#print(student_arr)
 for i in range(n):
     dicount = student_arr[i][0] // 2
     sum_stu = dicount + student_arr[i][1]
@@ -11,11 +13,12 @@ for i in range(n):
             continue
         sum_stu += (student_arr[j][0] + student_arr[j][1])
         if m - sum_stu >= 0:
-            stu_cnt += 1
-            #print(i,j,stu_cnt,answer)
+            stu_cnt += 1    
         else :
             break
+    #print(i+1,j+1,sum_stu,stu_cnt,answer)
     answer = max(answer, stu_cnt)
+    
 
 
 print(answer)
