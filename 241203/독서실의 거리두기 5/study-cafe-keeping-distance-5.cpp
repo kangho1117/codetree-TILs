925aa08225063982ff6d78fc14f2c3bae7104884
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-12-03 13:16:51 +0900

    Title: 독서실의 거리두기 5, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241203/독서실의 거리두기 5/study-cafe-keeping-distance-5.cpp
+++ 241203/독서실의 거리두기 5/study-cafe-keeping-distance-5.cpp
@@ -13,26 +13,28 @@ int main() {
         if(arr[i] == '0')
         {
             arr[i] = '1';
-            for(int j=i;j<n;j++)
+            for(int j=0;j<n;j++)
             {
+                if(arr[j]=='0') continue;
                 int cnt1=1,cnt2=1;
                 for(int a = j-1;a>=-1;a--)
                 {
                     if(arr[a] == '1') break;
-                    else if(a<=0) cnt1 = 100;
+                    else if(a<=0) cnt1 = 99;
                     cnt1++;
                 }
                 for(int b = j+1;b<=n;b++)
                 {
                     if(arr[b] == '1') break;
-                    else if(b>=n-1) cnt2 = 100;
+                    else if(b>=n-1) cnt2 = 99;
                     cnt2++;
                 }
                 if(cnt1 < cnt2 && cnt1 < cnt3) cnt3 = cnt1;
                 else if(cnt2 <= cnt1 && cnt2 < cnt3) cnt3 = cnt2;
-                //cout<<i<<" "<<j<<" "<<cnt1<<" "<<cnt2<<" "<<cnt3<<"\n";
-                if(cnt4 < cnt3) cnt4 = cnt3;
+                //cout<<i<<" "<<j<<" "<<cnt1<<" "<<cnt2<<" "<<cnt3<<" "<<cnt4<<"\n\n";
+                if(cnt3 == 1) break;
             }
+            if(cnt4 < cnt3) cnt4 = cnt3;
             arr[i] = '0';
         }
     }
