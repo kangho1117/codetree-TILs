a975081070da07aa71b3613bd3f006f59cbce33b
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-12-03 13:00:25 +0900

    Title: 독서실의 거리두기 5, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241203/독서실의 거리두기 5/study-cafe-keeping-distance-5.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin>>n;
+    char arr[n];
+    for(int i=0;i<n;i++)
+        cin>>arr[i];
+    int cnt4=0;
+    for(int i=0;i<n;i++)
+    {
+        int cnt3=100;
+        if(arr[i] == '0')
+        {
+            arr[i] = '1';
+            for(int j=i;j<n;j++)
+            {
+                int cnt1=1,cnt2=1;
+                for(int a = j-1;a>=-1;a--)
+                {
+                    if(arr[a] == '1') break;
+                    else if(a<=0) cnt1 = 100;
+                    cnt1++;
+                }
+                for(int b = j+1;b<=n;b++)
+                {
+                    if(arr[b] == '1') break;
+                    else if(b>=n-1) cnt2 = 100;
+                    cnt2++;
+                }
+                if(cnt1 < cnt2 && cnt1 < cnt3) cnt3 = cnt1;
+                else if(cnt2 <= cnt1 && cnt2 < cnt3) cnt3 = cnt2;
+                //cout<<i<<" "<<j<<" "<<cnt1<<" "<<cnt2<<" "<<cnt3<<"\n";
+                if(cnt4 < cnt3) cnt4 = cnt3;
+            }
+            arr[i] = '0';
+        }
+    }
+    cout<<cnt4;
+    return 0;
+}
