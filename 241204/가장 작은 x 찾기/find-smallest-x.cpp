c9bd190c31cf00617880c143216ff2e088c7a43e
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-12-04 10:35:53 +0900

    Title: 가장 작은 x 찾기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241204/가장 작은 x 찾기/find-smallest-x.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin>>n;
+    int arr[n][2];
+    for(int i=0;i<n;i++)
+    {
+        int a,b;
+        cin>>a>>b;
+        arr[i][0] = a;
+        arr[i][1] = b;
+    }
+    for(int i=1,ans;i<=100;i++)
+    {
+        ans = i;
+        bool success = false; 
+        for(int j=0;j<n;j++)
+        {
+            int a,b;
+            a = arr[j][0];
+            b = arr[j][1];
+            ans *=2;
+            if(a <= ans && ans <=b) success = true;
+            else   
+            {
+                success = false;
+                break;
+            }
+        }
+        if(success) cout<<i;
+
+    }
+    return 0;
+}
