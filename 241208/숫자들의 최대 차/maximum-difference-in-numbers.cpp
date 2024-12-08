9ab74ba7ccf660adf8e7cf6c782244a18856dbbf
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-12-08 14:21:01 +0900

    Title: 숫자들의 최대 차, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241208/숫자들의 최대 차/maximum-difference-in-numbers.cpp
+++ 241208/숫자들의 최대 차/maximum-difference-in-numbers.cpp
@@ -9,7 +9,15 @@ int main() {
         cin>>arr[i];
     sort(arr,arr+n);
     for(int i=0;i<n;i++)
-        if(arr[i]-arr[0]<=k) ans++;
+    {
+        int sum=0;
+        for(int j=0;j<=i;j++)
+        {
+            //cout<<arr[i]<<" "<<arr[j]<<" "<<arr[i]-arr[j]<<"\n";
+            if(arr[i]-arr[j]<=k) sum++;
+        }
+        if(sum > ans) ans = sum;
+    }
     cout<<ans;
     return 0;
 }
