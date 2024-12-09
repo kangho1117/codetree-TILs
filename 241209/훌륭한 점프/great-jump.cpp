337ca2021babb148ca570c4f36ca152ead7050fd
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-12-09 11:23:58 +0900

    Title: 훌륭한 점프, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241209/훌륭한 점프/great-jump.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n,k;
+    cin>>n>>k;
+    int arr[n];
+    for(int i=0;i<n;i++)
+        cin>>arr[i];
+    for(int i=1;i<=n;i++)
+    {
+        int cnt=0;
+        for(int j=0;j<n;j++)
+        {
+            if(arr[j]<=i) cnt=0;
+            else cnt++;
+            if(cnt>=k) break;
+        }
+        if(cnt<k) {
+            cout<<i;
+            break;
+        }
+    }
+    return 0;
+}
