1002142392bb3b24dfe26402e5a8479d688fa298
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-12-11 11:14:55 +0900

    Title: 독서실의 거리두기 4, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241211/독서실의 거리두기 4/study-cafe-keeping-distance-4.cpp
+++ 241211/독서실의 거리두기 4/study-cafe-keeping-distance-4.cpp
@@ -27,19 +27,16 @@ int main() {
                                 temp++;
                                 if(a>=n-1 && arr[n-1] == '0') cnt1=99;
                                 if(arr[a] == '1') {cnt1=temp;break;}
-                                
                             }
                             for(int b=i-1,temp=0;b>=0;b--)
                             {
                                 temp++;
                                 if(b<=0 && arr[0] == '0') cnt2=99;
                                 if(arr[b] == '1') {cnt2=temp;break;}
-                                
                             }
                             
                             if(cnt1<=cnt2 && cnt3 >= cnt1) cnt3 = cnt1;
                             else if(cnt1>=cnt2 && cnt3 >= cnt2) cnt3 = cnt2;
-                            //cout<<i<<" "<<cnt1<<" "<<cnt2<<" "<<cnt3<<" "<<ans<<"\n";
                         }
                     }
                     if(ans < cnt3) ans = cnt3;
