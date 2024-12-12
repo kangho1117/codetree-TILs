a3c1d82fe73af5d3ed55a01f7c49c1a90bdae476
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-12-12 10:12:05 +0900

    Title: 초기 수열 복원하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241212/초기 수열 복원하기/restore-initial-sequence.cpp
+++ 241212/초기 수열 복원하기/restore-initial-sequence.cpp
@@ -1,30 +1,36 @@
 #include <iostream>
 using namespace std;
 int main() {
-    int n;
-    cin>>n;
-    int arr[n];
-    for(int i=0;i<n-1;i++)
-        cin>>arr[i];
-    for(int i=1;arr[0]-i>0;i++)
-    {
-        int digit[2*(n+1)]={0,},temp[n]={0,};
-        temp[0] = i;
-        temp[1] = arr[0]-i;
-        if(temp[0] == temp[1]) continue;
-        digit[i] = 1;
-        digit[arr[0]-i] = 1;
-        for(int j=2;j<n;j++)
-        {
-            int num = arr[j-1]-temp[j-1];
-            if(digit[num] || num <= 0) {temp[n-1]=0;break;}
-            else temp[j] = num;
-        }
-        if(temp[n-1])
-        {
-            for(int k=0;k<n;k++)
-                cout<<temp[k]<<" ";
-        }
-    }
-    return 0;
+	int n;
+	cin>>n;
+	int arr[n];
+	for(int i=0; i<n-1; i++)
+		cin>>arr[i];
+	for(int i=1; arr[0]-i>0; i++)
+	{
+		int digit[2500]= {0,},temp[n+1]= {0,};
+		temp[0] = i;
+		temp[1] = arr[0]-i;
+		if(temp[0] == temp[1]) continue;
+		digit[i] = 1;
+		digit[arr[0]-i] = 1;
+		for(int j=2; j<n; j++)
+		{
+			int num = arr[j-1]-temp[j-1];
+			if(digit[num] || num <= 0 || num > n) {
+				temp[n-1]=0;
+				break;
+			}
+			else {
+				temp[j] = num;
+				digit[num]=1;
+			}
+		}
+		if(temp[n-1])
+		{
+			for(int k=0; k<n; k++)
+				cout<<temp[k]<<" ";
+		}
+	}
+	return 0;
 }
