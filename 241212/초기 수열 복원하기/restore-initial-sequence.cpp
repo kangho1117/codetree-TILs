dcf4e90202487b38413b4701f30e84e377ca84a0
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2024-12-12 10:01:35 +0900

    Title: 초기 수열 복원하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241212/초기 수열 복원하기/restore-initial-sequence.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n-1;i++)
+        cin>>arr[i];
+    for(int i=1;arr[0]-i>0;i++)
+    {
+        int digit[n+1]={0,},temp[n]={0,};
+        temp[0] = i;
+        temp[1] = arr[0]-i;
+        if(temp[0] == temp[1]) continue;
+        digit[i] = 1;
+        digit[arr[0]-i] = 1;
+        for(int j=2;j<n;j++)
+        {
+            int num = arr[j-1]-temp[j-1];
+            if(digit[num] || num <= 0) {temp[n-1]=0;break;}
+            else temp[j] = num;
+        }
+        if(temp[n-1])
+        {
+            for(int k=0;k<n;k++)
+                cout<<temp[k]<<" ";
+        }
+    }
+    return 0;
+}
