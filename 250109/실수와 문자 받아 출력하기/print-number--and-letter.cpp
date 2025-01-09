1ba553251ea906e144674f47ea575a97197c218e
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2025-01-09 09:52:00 +0900

    Title: 실수와 문자 받아 출력하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250109/실수와 문자 받아 출력하기/print-number--and-letter.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    char c;
+    float a,b;
+    cin>>c>>a>>b;
+    cout.precision(2);
+    cout<<fixed<<c<<"\n"<<a<<"\n"<<b;
+    return 0;
+}
