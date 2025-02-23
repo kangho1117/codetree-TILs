95f33ee00e0835bc2bf11830a59b3c07da5a121a
Bae Gwangho <93529538+kangho1117@users.noreply.github.com>
2025-02-23 18:41:53 +0900

    Title: 비교 연산, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250223/비교 연산/comparison-operator.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a,b;
+    cin>>a>>b;
+    if(a>=b) cout<<"1\n";
+    else cout<<"0\n";
+    if(a>b) cout<<"1\n";
+    else cout<<"0\n";
+    if(b>=a) cout<<"1\n";
+    else cout<<"0\n";
+    if(b>a) cout<<"1\n";
+    else cout<<"0\n";
+    if(a==b) cout<<"1\n";
+    else cout<<"0\n";
+    if(a!=b) cout<<"1\n";
+    else cout<<"0\n";
+    return 0;
+}
