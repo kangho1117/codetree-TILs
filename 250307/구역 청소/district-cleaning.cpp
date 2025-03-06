129485d0d525876c321c66aa12bc3a69825a6625
Bae Kwangho <93529538+kangho1117@users.noreply.github.com>
2025-03-07 03:22:42 +0900

    Title: 구역 청소, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 250307/구역 청소/district-cleaning.cpp
+++ 250307/구역 청소/district-cleaning.cpp
@@ -8,8 +8,10 @@ int main() {
     cin >> a >> b;
     cin >> c >> d;
 
-    if(a<c && c<b) cout<<d-a;
+    if(a<d && d<b) cout<<b-c;
+    else if(a<c && c<b) cout<<d-a;
     else if(c<a && a<d) cout<<b-c;
+    else if(c<b && b<d) cout<<d-a;
     else cout<<(b-a)+(d-c);
 
     return 0;
