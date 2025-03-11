0cf654bc1afb3ea12ec73d7009b30520610b8972
Bae Kwangho <93529538+kangho1117@users.noreply.github.com>
2025-03-12 00:40:39 +0900

    Title: 전부 겹치는 선분 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 250312/전부 겹치는 선분 2/overlapping-line-segments-2.cpp
+++ 250312/전부 겹치는 선분 2/overlapping-line-segments-2.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 using namespace std;
 int n;
-int x1[100], x2[100],arr[101];
+int x1[100], x2[100];
 bool yes = false;
 int main() {
     cin >> n;
     for (int i = 0; i < n; i++) 
         cin >> x1[i] >> x2[i];
     for (int i = 0; i < n; i++) {
+        int  arr[101] = {0,};
         for(int j=0; j<n; j++)
         {
             if(i==j) continue;
@@ -18,7 +19,7 @@ int main() {
         if(arr[i]==n-1)
             yes = true;
     }
-    if(yes)cout<<"Yes";
+    if(yes) cout<<"Yes";
     else cout<<"No";
     return 0;
 }
