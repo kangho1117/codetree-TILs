38f739b2951a209c9846372f970ef642f515c230
Bae Kwangho <93529538+kangho1117@users.noreply.github.com>
2025-03-15 02:01:19 +0900

    Title: 달리자, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250315/달리자/run-and-run.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int n,sum=0;
+int A[100];
+int B[100];
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; i++)
+        cin >> A[i];
+    for (int i = 0; i < n; i++)
+        cin >> B[i];
+    for (int i = 0; i < n; i++)
+    {
+        while(A[i]!=B[i])
+        {
+            A[i]--;
+            A[i+1]++;
+            sum++;
+        }
+    }
+    cout<<sum;
+    return 0;
+}
