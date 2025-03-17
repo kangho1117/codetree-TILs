e7d4b6ef6431d949d6cb7cd5d44ecd1770975095
Bae Kwangho <93529538+kangho1117@users.noreply.github.com>
2025-03-17 19:56:07 +0900

    Title: 코딩톡, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250317/코딩톡/coding-talk.cpp
+++ 250317/코딩톡/coding-talk.cpp
@@ -1,24 +1,38 @@
 #include <iostream>
-#include <vector>
+
+#define MAX_M 100
+
 using namespace std;
 
-int n, m, p;
-char c[100];
-int u[100];
+int n, p, m;
+char c[MAX_M];
+int u[MAX_M];
+
 int main() {
+    // 입력
     cin >> n >> m >> p;
-    vector<bool> people(n,false);
-    for (int i = 0; i < m; i++) {
+    for(int i = 0; i < m; i++)
         cin >> c[i] >> u[i];
+
+    // 모두 읽은 채팅이라면 읽지 않은 사람은 없습니다.
+    if(u[p - 1] == 0)
+        return 0;
+    
+    // 각 사람에 대해 채팅을 읽었을지 안 읽었을지 판단합니다.
+    for(int i = 0; i < n; i++) {
+        // read : 확실하게 채팅을 읽었으면 true
+        char person = 'A' + i;
+        bool read = false;
+
+        // 만약 p번 메시지를 읽은 사람 수와 같은 채팅을 기준으로
+        // 한번이라도 채팅을 쳤다면 확실하게 채팅을 읽었습니다.
+        for(int j = 0; j < m; j++)
+            if(u[j] >= u[p - 1] && c[j] == person)
+                read = true;
+            
+        if(read == false)
+            cout << person << " ";
     }
-    for (int i = p-1; i < m; i++) {
-        people[c[i]-65] = true;
-    }
-    for(int i= p-1; i>0; i--)
-    {
-        if(u[i-1] == u[i]) people[c[i-1]-65] = true;
-    }
-    for(int i=0;i<n;i++)
-        if(people[i]==0) cout<<char(i+65)<<" ";
+    
     return 0;
 }
