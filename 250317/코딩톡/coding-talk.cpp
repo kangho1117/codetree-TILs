c9a39d7e64562140bbaa801236a19b18ab82bd14
Bae Kwangho <93529538+kangho1117@users.noreply.github.com>
2025-03-17 19:44:10 +0900

    Title: 코딩톡, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 250317/코딩톡/coding-talk.cpp
+++ 250317/코딩톡/coding-talk.cpp
@@ -7,14 +7,15 @@ char c[100];
 int u[100];
 int main() {
     cin >> n >> m >> p;
-    vector<bool> people(p,false);
+    vector<bool> people(n,false);
     for (int i = 0; i < m; i++) {
         cin >> c[i] >> u[i];
     }
     for (int i = p-1; i < m; i++) {
         people[c[i]-65] = true;
     }
-    for(int i=0;i<m;i++)
+    if(u[p-2] == u[p-1]) people[c[p-2]-65] = true;
+    for(int i=0;i<n;i++)
         if(people[i]==0) cout<<char(i+65)<<" ";
     
 
