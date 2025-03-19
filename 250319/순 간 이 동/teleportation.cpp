2559cf2aa90119cf476ae7d2200ca788fb222feb
Bae Kwangho <93529538+kangho1117@users.noreply.github.com>
2025-03-19 15:09:43 +0900

    Title: 순 간 이 동, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250319/순 간 이 동/teleportation.cpp
+++ 250319/순 간 이 동/teleportation.cpp
@@ -2,12 +2,9 @@
 #include <cstdlib>
 #include <algorithm>
 using namespace std;
-int a, b, x, y,minans=1000;
+int a, b, x, y;
 int main() {
     cin >> a >> b >> x >> y;
-    if(minans > abs(a-b)) minans = abs(a-b);
-    if(minans > abs(a-x)+abs(y-b)) minans = abs(a-x)+abs(y-b);
-    if(minans > abs(a-y)+abs(x-b)) minans = abs(a-y)+abs(x-b);
-    cout<<minans;
+    cout<<min(abs(a-b), min(abs(a-x)+abs(y-b), abs(a-y)+abs(x-b)));
     return 0;
 }
