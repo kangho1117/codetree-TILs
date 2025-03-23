8b618865161478753b93e38c1552e74ab96d4492
Bae Kwangho <93529538+kangho1117@users.noreply.github.com>
2025-03-23 15:39:19 +0900

    Title: 독서실의 거리두기 3, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250323/독서실의 거리두기 3/study-cafe-keeping-distance-3.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int N;
+string seats;
+int main() {
+    cin >> N;
+    cin >> seats;
+    int a=0,b=-1,mlen=0,ma,mb;
+    for(int i=1;i<seats.size();i++)
+    {   
+        if(seats[i] == '1' && b==-1)
+        {
+            b = i;
+            if(mlen < b-a) 
+            {
+                mlen = b-a;
+                ma = a;
+                mb = b;
+                a = b;
+                b = -1;
+            }
+        }
+    }
+    seats[(ma+mb)/2] = '1';
+    a=0,b=-1,mlen=1000,ma,mb;
+    for(int i=1;i<seats.size();i++)
+    {   
+        if(seats[i] == '1' && b==-1)
+        {
+            b = i;
+            if(mlen > b-a) 
+            {
+                mlen = b-a;
+                ma = a;
+                mb = b;
+                a = b;
+                b = -1;
+            }
+        }
+    }
+    //cout<<seats<<"\n";
+    cout<<mlen;
+    return 0;
+}
