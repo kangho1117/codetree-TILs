4b8a6851e1a0ebbbd6645ff285919b3a34441d4d
Bae Kwangho <93529538+kangho1117@users.noreply.github.com>
2025-03-26 22:34:30 +0900

    Title: 개발자의 가위바위보, Time: 6ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250326/개발자의 가위바위보/developers-rock-paper-scissors.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+using namespace std;
+int N;
+int a[100], b[100],ans=0;
+int main() {
+    cin >> N;
+    for (int i = 0; i < N; i++) 
+        cin >> a[i] >> b[i];
+    for (int i = 0, aa=0, bb=0; i < N; i++)
+    {//1 가위 2 보자기 3 주먹
+        if(a[i]==1)
+        {
+            if(b[i]==2) aa++;
+            else if(b[i]==3) bb++;
+        }
+        else if(a[i]==2)
+        {
+            if(b[i]==1) bb++;
+            else if(b[i]==3) aa++;
+        }
+        else if(a[i]==3)
+        {
+            if(b[i]==1) aa++;
+            else if(b[i]==2) bb++;
+        }
+        if(ans < aa) ans = aa;
+    }
+    for (int i = 0, aa=0, bb=0; i < N; i++)
+    {//1 주먹 2 보자기 3 가위 
+        if(a[i]==1)
+        {
+            if(b[i]==2) bb++;
+            else if(b[i]==3) aa++;
+        }
+        else if(a[i]==2)
+        {
+            if(b[i]==1) aa++;
+            else if(b[i]==3) bb++;
+        }
+        else if(a[i]==3)
+        {
+            if(b[i]==1) bb++;
+            else if(b[i]==2) aa++;
+        }
+        if(ans < aa) ans = aa;
+    }
+    for (int i = 0, aa=0, bb=0; i < N; i++)
+    {//1 보자기 2 가위 3 주먹
+        if(a[i]==1)
+        {
+            if(b[i]==2) bb++;
+            else if(b[i]==3) aa++;
+        }
+        else if(a[i]==2)
+        {
+            if(b[i]==1) aa++;
+            else if(b[i]==3) bb++;
+        }
+        else if(a[i]==3)
+        {
+            if(b[i]==1) aa++;
+            else if(b[i]==2) bb++;
+        }
+        if(ans < aa) ans = aa;
+    }
+    cout<<ans;
+    return 0;
+}
