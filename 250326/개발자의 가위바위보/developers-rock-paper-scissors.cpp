6f835d47fe81441dccdbd9e271355bf323a9d932
Bae Kwangho <93529538+kangho1117@users.noreply.github.com>
2025-03-26 22:35:01 +0900

    Title: 개발자의 가위바위보, Time: 6ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 250326/개발자의 가위바위보/developers-rock-paper-scissors.cpp
+++ 250326/개발자의 가위바위보/developers-rock-paper-scissors.cpp
@@ -4,8 +4,30 @@ int N;
 int a[100], b[100],ans=0;
 int main() {
     cin >> N;
-    for (int i = 0; i < N; i++) 
+    for (int i = 0; i < N; i++) {
         cin >> a[i] >> b[i];
+    }
+
+    for (int i = 0, aa=0, bb=0; i < N; i++)
+    {//1 가위 2 주먹 3 보자기
+        if(a[i]==1)
+        {
+            if(b[i]==2) bb++;
+            else if(b[i]==3) aa++;
+        }
+        else if(a[i]==2)
+        {
+            if(b[i]==1) aa++;
+            else if(b[i]==3) bb++;
+        }
+        else if(a[i]==3)
+        {
+            if(b[i]==1) bb++;
+            else if(b[i]==2) aa++;
+        }
+        if(ans < aa) ans = aa;
+    }
+
     for (int i = 0, aa=0, bb=0; i < N; i++)
     {//1 가위 2 보자기 3 주먹
         if(a[i]==1)
@@ -25,6 +47,27 @@ int main() {
         }
         if(ans < aa) ans = aa;
     }
+
+    for (int i = 0, aa=0, bb=0; i < N; i++)
+    {//1 주먹 2 가위 3 보자기 
+        if(a[i]==1)
+        {
+            if(b[i]==2) aa++;
+            else if(b[i]==3) bb++;
+        }
+        else if(a[i]==2)
+        {
+            if(b[i]==1) bb++;
+            else if(b[i]==3) aa++;
+        }
+        else if(a[i]==3)
+        {
+            if(b[i]==1) aa++;
+            else if(b[i]==2) bb++;
+        }
+        if(ans < aa) ans = aa;
+    }
+
     for (int i = 0, aa=0, bb=0; i < N; i++)
     {//1 주먹 2 보자기 3 가위 
         if(a[i]==1)
@@ -44,6 +87,7 @@ int main() {
         }
         if(ans < aa) ans = aa;
     }
+
     for (int i = 0, aa=0, bb=0; i < N; i++)
     {//1 보자기 2 가위 3 주먹
         if(a[i]==1)
@@ -63,6 +107,25 @@ int main() {
         }
         if(ans < aa) ans = aa;
     }
-    cout<<ans;
+
+    for (int i = 0, aa=0, bb=0; i < N; i++)
+    {//1 보자기 2 주먹 3 가위 
+        if(a[i]==1)
+        {
+            if(b[i]==2) aa++;
+            else if(b[i]==3) bb++;
+        }
+        else if(a[i]==2)
+        {
+            if(b[i]==1) bb++;
+            else if(b[i]==3) aa++;
+        }
+        else if(a[i]==3)
+        {
+            if(b[i]==1) aa++;
+            else if(b[i]==2) bb++;
+        }
+        if(ans < aa) ans = aa;
+    }
     return 0;
 }
