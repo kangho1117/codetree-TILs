417451beaa1a73efc2b5974e46b5dbc36ab04577
Bae Kwangho <93529538+kangho1117@users.noreply.github.com>
2025-04-04 00:07:05 +0900

    Title: 일 수 구하기, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250404/일 수 구하기/number-of-days-in-month.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin>>n;
+    if(n==1) cout<<31;
+    if(n==2) cout<<28;
+    if(n==3) cout<<31;
+    if(n==4) cout<<30;
+    if(n==5) cout<<31;
+    if(n==6) cout<<30;
+    if(n==7) cout<<31;
+    if(n==8) cout<<31;
+    if(n==9) cout<<30;
+    if(n==10) cout<<31;
+    if(n==11) cout<<30;
+    if(n==12) cout<<31;
+    return 0;
+}
