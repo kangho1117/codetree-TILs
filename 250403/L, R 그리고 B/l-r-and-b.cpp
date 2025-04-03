b3d15d870bcd1796d3f35f30cf3fddd0bb188c2b
Bae Kwangho <93529538+kangho1117@users.noreply.github.com>
2025-04-03 18:08:30 +0900

    Title: L, R 그리고 B, Time: 278ms, Memory: 80MB, Status: Memory Limit Exceed - Codetree

--- 250403/L, R 그리고 B/l-r-and-b.cpp
+++ 250403/L, R 그리고 B/l-r-and-b.cpp
@@ -1,40 +1,43 @@
 #include <iostream>
-#include <cstdlib>
+#include <vector>
+#include <queue>
+#include <tuple>
 using namespace std;
-char board[10][10];
-int bx,by,rx,ry,lx,ly;
+vector<vector<char>> v(12,vector<char>(12,'!'));
 int main() {
-    for (int i = 0; i < 10; i++) {
-        for (int j = 0; j < 10; j++) {
-            cin >> board[i][j];
-            if(board[i][j]=='B')
+    int sx,sy,sum=0;
+    for(int i=1;i<=10;i++)
+    {
+        for(int j=1;j<=10;j++)
+        {
+            cin>>v[i][j];
+            if(v[i][j] == 'B')
             {
-                bx = i;
-                by = j;
-            }
-            if(board[i][j]=='R')
-            {
-                rx = i;
-                ry = j;
-            }
-            if(board[i][j]=='L')
-            {
-                lx = i;
-                ly = j;
+                sx = i;
+                sy = j;
             }
         }
     }
-    if(bx == rx && rx == lx && ((bx<rx && rx < lx) || (lx < rx && rx < bx)))
+    queue<tuple<int,int,int>> q;
+    q.push({sx,sy,0});
+    while(!q.empty())
     {
-        cout<<abs(lx-bx) + abs(ly-by) -1 +2;
-    }
-    else if(by == ry && ry == ly && ((bx<rx && rx < lx) || (lx < rx && rx < bx)))
-    {
-        cout<<abs(lx-bx) + abs(ly-by) -1 +2;
-    }
-    else
-    {
-        cout<<abs(lx-bx) + abs(ly-by) -1;
+        int nowx = get<0>(q.front()), nowy = get<1>(q.front()), sum = get<2>(q.front());
+        q.pop();
+        if(v[nowx][nowy] == 'L')
+        {
+            cout<<sum-1;
+            break;
+        }
+        else if(v[nowx][nowy] == '!' || v[nowx][nowy] == 'R') continue;
+        else
+        {
+            v[nowx][nowy] = '.';
+            q.push({nowx+1,nowy,sum+1});
+            q.push({nowx-1,nowy,sum+1});
+            q.push({nowx,nowy+1,sum+1});
+            q.push({nowx,nowy-1,sum+1});
+        }
     }
     return 0;
 }
