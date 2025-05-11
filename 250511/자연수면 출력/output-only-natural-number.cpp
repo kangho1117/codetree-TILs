baabf3acc75aca8939e8addbf10989fa7103884c
Bae Kwangho <93529538+kangho1117@users.noreply.github.com>
2025-05-11 14:08:12 +0900

    Title: 자연수면 출력, Time: 6ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 250511/자연수면 출력/output-only-natural-number.cpp
+++ 250511/자연수면 출력/output-only-natural-number.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main() {
     int a,b;
     cin>>a>>b;
-    if(a<0) cout<<0;
+    if(a<=0) cout<<0;
     else if(a%2==0)
         for(int i=0;i<b;i++) cout<<a;
     return 0;
