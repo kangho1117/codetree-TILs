ae1ea784c8661702dfab4bccfa2d3e12bd0569a7
Bae Kwangho <93529538+kangho1117@users.noreply.github.com>
2025-05-18 01:22:39 +0900

    Title: 369 게임, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

--- 250518/369 게임/369-game.cpp
+++ 250518/369 게임/369-game.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
+#include <string>
 using namespace std;
 int main() {
     int n;
     cin>>n;
     for(int i=1;i<=n;i++)
     {
-        if(i%3==0 || i/100 == 3 || i%100/10==3 || i%10==3) cout<<0<<" ";
+        string s = to_string(i);
+        if(i%3==0)
+        {
+            cout<<0<<" ";
+        }
+        else if(s.size()==2)
+        {
+            if(s[0]=='3' || s[0]=='6' || s[0]=='9' || s[1]=='3' || s[1]=='6' || s[1]=='9') cout<<0<<" ";
+            else cout<<i<<" ";
+        }
         else cout<<i<<" ";
     }
     return 0;
