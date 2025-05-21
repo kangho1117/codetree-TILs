891a23012cdefee98a687bb355b318afd7f1ef94
Bae Kwangho <93529538+kangho1117@users.noreply.github.com>
2025-05-21 17:30:21 +0900

    Title: 배수의 개수, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

--- 250521/배수의 개수/number-of-multipliers.cpp
+++ 250521/배수의 개수/number-of-multipliers.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 using namespace std;
 int main() {
-    int cnt=0;
-    for(int i=0;i<10;i++) if(i%3==0 || i%5==0) cnt++;
-    cout<<cnt;
+    int cnt1=0,cnt2=0;
+    for(int i=0,t;i<10;i++) {
+        cin>>t;
+        if(t%3==0) cnt1++;
+        if(t%5==0) cnt2++;
+    }
+    cout<<cnt1<<" "<<cnt2;
     return 0;
 }
