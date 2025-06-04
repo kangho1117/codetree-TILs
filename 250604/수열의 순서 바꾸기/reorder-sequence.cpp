d034b3e3048ea8d294706f4002adf0e0b98b6dd1
Bae Kwangho <93529538+kangho1117@users.noreply.github.com>
2025-06-04 15:10:30 +0900

    Title: 수열의 순서 바꾸기, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 250604/수열의 순서 바꾸기/reorder-sequence.cpp
+++ 250604/수열의 순서 바꾸기/reorder-sequence.cpp
@@ -4,62 +4,26 @@
 using namespace std;
 int main()
 {
-    int n,cnt=0;
+    int n,cnt=0,temp=0;
     cin>>n;
-    deque<int> cmp,dq1,dq2;
+    deque<int> dq;
     for(int i=0,temp;i<n;i++)
     {
         cin>>temp;
-        dq1.push_back(temp);
+        dq.push_back(temp);
     }
-    cmp = dq1;
-    sort(cmp.begin(), cmp.end());
-    bool loop = true;
-    while(loop)
+    for(auto it = dq.begin(); it != dq.end(); it++)
     {
-        deque<int> t1 = dq1, t2 = dq2, sum;
-        while(!(t1.empty() && t2.empty()))
+        if(temp < *it)
         {
-            if(t2.empty())
-            {
-                sum.push_back(t1.front());
-                t1.pop_front();
-            }
-            else if(t1.empty())
-            {
-                sum.push_back(t2.front());
-                t2.pop_front();
-            }
-            else if(t1.front() < t2.front())
-            {
-                sum.push_back(t1.front());
-                t1.pop_front();
-            }
-            else
-            {
-                sum.push_back(t2.front());
-                t2.pop_front();
-            }
+            temp = *it;
         }
-        // cout<<"sum : ";
-        // for(auto a : sum) cout<<a<<" ";
-        // cout<<"\n";
-        if(sum == cmp) loop = false;
         else
         {
-            if(!dq1.empty())
-            {
-                dq2.push_back(dq1.front());
-                dq1.pop_front();
-                cnt++;
-            }
+            cnt++;
+            dq.pop_front();
+            it = dq.begin();
         }
-        // cout<<"dq1 : ";
-        // for(auto a : dq1) cout<<a<<" ";
-        // cout<<"\n";
-        // cout<<"dq2 : ";
-        // for(auto a : dq2) cout<<a<<" ";
-        // cout<<"\n";
     }
     cout<<cnt;
     return 0;
