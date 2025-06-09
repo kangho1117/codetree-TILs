594ca2cd3733a0843ea92f7c4df6c6fce61b9dbe
Bae Kwangho <93529538+kangho1117@users.noreply.github.com>
2025-06-09 11:59:39 +0900

    Title: A, B, C, D 찾기, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

--- 250609/A, B, C, D 찾기/find-a-b-c-d.cpp
+++ 250609/A, B, C, D 찾기/find-a-b-c-d.cpp
@@ -1,18 +1,16 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
-int arr[15],a,b,c,d,ab,bc,cd,da,ac,bd,abc,abd,acd,bcd,abcd;
+int arr[15],a,b,c,d,abcd;
 int main() {
     for (int i = 0; i < 15; i++)
         cin >> arr[i];
     sort(arr,arr+15);
     a = arr[0];
     b = arr[1];
-    abcd = arr[14];
-    acd = abcd-b;
-    cd = acd - a;
     c = arr[2];
-    d = cd-c;
+    abcd = arr[14];
+    d = abcd-a-b-c;
     cout<<a<<" "<<b<<" "<<c<<" "<<d;
     return 0;
 }
