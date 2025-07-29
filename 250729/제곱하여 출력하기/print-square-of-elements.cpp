8a6b25e453a4580f4f9e9dd87defe2c6e5e3a889
Bae Kwangho <93529538+kangho1117@users.noreply.github.com>
2025-07-29 10:29:33 +0900

    Title: 제곱하여 출력하기, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250729/제곱하여 출력하기/print-square-of-elements.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    cin>>n;
+    for(int i=0,j;i<n;i++)
+    {
+        cin>>j;
+        cout<<j*j<<" ";
+    }
+    return 0;
+}
